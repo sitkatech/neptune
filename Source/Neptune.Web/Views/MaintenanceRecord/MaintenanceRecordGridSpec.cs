159b12d17a1e3cf4f7ce58367b2589c17cafcf8a
Nick <nick.padinha@sitkatech.com>
2018-05-08 14:19:10 -0700

    [neptune/#69] Build maintenance tracking field data input Finish up with the Date Time picker work.

--- Source/Neptune.Web/Views/MaintenanceRecord/MaintenanceRecordGridSpec.cs
+++ Source/Neptune.Web/Views/MaintenanceRecord/MaintenanceRecordGridSpec.cs
@@ -45,7 +45,7 @@ namespace Neptune.Web.Views.MaintenanceRecord
                 x => DhtmlxGridHtmlHelpers.MakeEditIconAsModalDialogLinkBootstrap(new ModalDialogForm(x.GetEditUrl(),
                         $"Edit {Models.FieldDefinition.MaintenanceRecord.GetFieldDefinitionLabel()}"), currentPersonCanEditOrDelete), 30, DhtmlxGridColumnFilterType.None);
 
-            Add("Date", x => x.MaintenanceRecordDate.ToShortDateString(), 100);
+            Add("Date", x => x.MaintenanceRecordDate.ToString("g"), 100);
             Add("Performed By", x => x.PerformedByOrganization.OrganizationShortName, 100, DhtmlxGridColumnFilterType.Text);
             Add("Entered By", x => x.EnteredByPerson.FullNameLastFirst, 100, DhtmlxGridColumnFilterType.Text);
             Add(Models.FieldDefinition.MaintenanceRecordType.ToGridHeaderString("Type"),
