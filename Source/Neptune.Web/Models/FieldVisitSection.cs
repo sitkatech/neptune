f5d37185dc4373dd67a86e1a198829a51635d7e3
Nick <nick.padinha@sitkatech.com>
2018-05-23 15:55:28 -0700

    [neptune/#106] Field Visit workflow - Initial workflow Added GetSectionUrl to FieldVisitSection abstract class and wired it into the FV/SiteLayout.cshtml

---
+++ Source/Neptune.Web/Models/FieldVisitSection.cs
@@ -0,0 +1,58 @@
+using Neptune.Web.Common;
+using Neptune.Web.Controllers;
+
+namespace Neptune.Web.Models
+{
+    public abstract partial class FieldVisitSection
+    {
+        public abstract string GetSectionUrl(FieldVisit fieldVisit);
+    }
+
+    public partial class FieldVisitSectionInventory : FieldVisitSection
+    {
+        public override string GetSectionUrl(FieldVisit fieldVisit)
+        {
+            return SitkaRoute<FieldVisitController>.BuildUrlFromExpression(x => x.Inventory(fieldVisit));
+        }
+    }
+
+    public partial class FieldVisitSectionAssessment : FieldVisitSection
+    {
+        public override string GetSectionUrl(FieldVisit fieldVisit)
+        {
+            return SitkaRoute<FieldVisitController>.BuildUrlFromExpression(x => x.Assess(fieldVisit));
+        }
+    }
+
+    public partial class FieldVisitSectionMaintain : FieldVisitSection
+    {
+        public override string GetSectionUrl(FieldVisit fieldVisit)
+        {
+            return SitkaRoute<FieldVisitController>.BuildUrlFromExpression(x => x.Maintain(fieldVisit));
+        }
+    }
+
+    public partial class FieldVisitSectionPostMaintenanceAssessment : FieldVisitSection
+    {
+        public override string GetSectionUrl(FieldVisit fieldVisit)
+        {
+            return SitkaRoute<FieldVisitController>.BuildUrlFromExpression(x => x.PostMaintenanceAssessment(fieldVisit));
+        }
+    }
+
+    public partial class FieldVisitSectionWrapUpVisit : FieldVisitSection
+    {
+        public override string GetSectionUrl(FieldVisit fieldVisit)
+        {
+            return SitkaRoute<FieldVisitController>.BuildUrlFromExpression(x => x.WrapUpVisit(fieldVisit));
+        }
+    }
+
+    public partial class FieldVisitSectionManageVisit : FieldVisitSection
+    {
+        public override string GetSectionUrl(FieldVisit fieldVisit)
+        {
+            return SitkaRoute<FieldVisitController>.BuildUrlFromExpression(x => x.ManageVisit(fieldVisit));
+        }
+    }
+}
