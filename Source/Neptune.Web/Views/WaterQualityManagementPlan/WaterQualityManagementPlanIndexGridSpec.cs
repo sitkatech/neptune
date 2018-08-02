61377cffa841c9c78141a38c13551aaad53f34ac
Nick <nick.padinha@sitkatech.com>
2018-08-02 12:54:09 -0700

    [neptune/#135] Define WQMP document types and rules Fix the grid

--- Source/Neptune.Web/Views/WaterQualityManagementPlan/WaterQualityManagementPlanIndexGridSpec.cs
+++ Source/Neptune.Web/Views/WaterQualityManagementPlan/WaterQualityManagementPlanIndexGridSpec.cs
@@ -53,13 +53,13 @@ namespace Neptune.Web.Views.WaterQualityManagementPlan
             Add("Maintenance Contact Organization", x => x.MaintenanceContactOrganization, 120);
             Add("Maintenance Contact Address", x => x.MaintenanceContactAddressToString(), 200);
             Add("Maintenance Contact Phone", x => x.MaintenanceContactPhone, 70);
-            Add("# of Treatment BMPs", x => x.TreatmentBMPs.Count, 50);
-            Add("# of Documents", x => x.WaterQualityManagementPlanDocuments.Count, 50);
+            Add("# of Treatment BMPs", x => x.TreatmentBMPs.Count, 100);
+            Add("# of Documents", x => x.WaterQualityManagementPlanDocuments.Count, 100);
             Add(Models.FieldDefinition.HasAllRequiredDocuments.ToGridHeaderString(),
                 x => x.HasAllRequiredDocuments()
                     ? BootstrapHtmlHelpers.MakeGlyphIconWithHiddenText("glyphicon-ok-circle", "Yes")
                     : new HtmlString("<span style='display:none;'>No</span>")
-                , 50, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict, DhtmlxGridColumnAlignType.Center);
+                , 100, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict, DhtmlxGridColumnAlignType.Center);
         }
     }
 }
