4036d3f9bcfb3a9fd31a6dc07e7038fdd18e100b
jenna mccarter <jenna.mccarter@sitkatech.com>
2018-08-29 12:05:57 -0700

    [neptune/#150] Manager dashboard showing BMPs and Field Visits requiring approval
    * On the BMP grid, the View link now goes to the BMP Detail page for that BMP
    * Added “Date of Last Inventory Change during Field Visit” as a Field Definition and set the text as “Date of Last Inventory Change during Field Visit”
    * Moved the "Jurisdiction" column to be the last column

--- Source/Neptune.Web/Views/ManagerDashboard/ProvisionalTreatmentBMPGridSpec.cs
+++ Source/Neptune.Web/Views/ManagerDashboard/ProvisionalTreatmentBMPGridSpec.cs
@@ -36,16 +36,14 @@ namespace Neptune.Web.Views.ManagerDashboard
         public ProvisionalTreatmentBMPGridSpec(Person currentPerson)
         {
             Add(string.Empty, x => DhtmlxGridHtmlHelpers.MakeDeleteIconAndLinkBootstrap(x.GetDeleteUrl(), x.CanDelete(currentPerson), x.CanDelete(currentPerson)), 30, DhtmlxGridColumnFilterType.None);
-            Add(string.Empty, x => UrlTemplate.MakeHrefString(SitkaRoute<FieldVisitController>.BuildUrlFromExpression(y => y.Inventory(x.PrimaryKey)), "View", new Dictionary<string, string> { { "class", "gridButton" } }), 50, DhtmlxGridColumnFilterType.None);
+            Add(string.Empty, x => UrlTemplate.MakeHrefString(SitkaRoute<TreatmentBMPController>.BuildUrlFromExpression(y => y.Detail(x.PrimaryKey)), "View", new Dictionary<string, string> { { "class", "gridButton" } }), 50, DhtmlxGridColumnFilterType.None);
             Add("BMP Name", x => x.GetDisplayNameAsUrl(), 120, DhtmlxGridColumnFilterType.Html);
-            Add(Models.FieldDefinition.Jurisdiction.ToGridHeaderString(), x => x.StormwaterJurisdiction.GetDisplayNameAsDetailUrl(), 140, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
-
             Add(Models.FieldDefinition.TreatmentBMPType.ToGridHeaderString(), x => x.TreatmentBMPType.TreatmentBMPTypeName, 180, DhtmlxGridColumnFilterType.SelectFilterStrict);
             Add("Date of Last Verification", x => x.DateOfLastInventoryVerification, 125);
-            Add("Date of Last Field Visit", x => x.GetLastFieldVisitWithAnInventoryUpdate()?.VisitDate, 120);
-
+            Add(Models.FieldDefinition.DateOfLastInventoryChangeDuringFieldVisit.ToGridHeaderString(), x => x.GetLastFieldVisitWithAnInventoryUpdate()?.VisitDate, 120);
             Add("Has Photos?", x => x.TreatmentBMPImages.Any().ToYesNo(), 120, DhtmlxGridColumnFilterType.SelectFilterStrict);
             Add("Benchmark and Thresholds Set?", x => x.IsBenchmarkAndThresholdsComplete().ToString(), 120, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add(Models.FieldDefinition.Jurisdiction.ToGridHeaderString(), x => x.StormwaterJurisdiction.GetDisplayNameAsDetailUrl(), 140, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
         }
     }
 }
