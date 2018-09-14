55c3bf0260a111ad105467e4680931a1b4d1d573
Nick <nick.padinha@sitkatech.com>
2018-09-13 17:58:24 -0700

    [neptune/#169] Block editing Verified Field Visits & allow deleting Assessments/Maintenance records Maintenance and Summary tabs

--- Source/Neptune.Web/Views/FieldVisit/DetailViewData.cs
+++ Source/Neptune.Web/Views/FieldVisit/DetailViewData.cs
@@ -1,7 +1,10 @@
-﻿using LtInfo.Common;
+﻿using System.Collections;
+using System.Linq;
+using LtInfo.Common;
 using Neptune.Web.Common;
 using Neptune.Web.Controllers;
 using Neptune.Web.Models;
+using Neptune.Web.Security;
 using Neptune.Web.Views;
 
 namespace Neptune.Web.Views.FieldVisit
@@ -9,6 +12,9 @@ namespace Neptune.Web.Views.FieldVisit
     public class DetailViewData : NeptuneViewData
     {
         public Models.FieldVisit FieldVisit { get; }
+        public bool UserCanDeleteMaintenanceRecord { get; }
+        public bool UserHasCustomAttributeTypeManagePermissions { get;  }
+        public IOrderedEnumerable<MaintenanceRecordObservation> SortedMaintenanceRecordObservations { get; }
 
         public DetailViewData(Person currentPerson, StormwaterBreadCrumbEntity stormwaterBreadCrumbEntity,
             Models.FieldVisit fieldVisit) : base(currentPerson, stormwaterBreadCrumbEntity)
@@ -19,6 +25,12 @@ namespace Neptune.Web.Views.FieldVisit
             SubEntityName = FieldVisit.TreatmentBMP.TreatmentBMPName ?? "Preview Treatment BMP Field Visit";
             SubEntityUrl = FieldVisit.TreatmentBMP?.GetDetailUrl() ?? "#";
             PageTitle = FieldVisit.VisitDate.ToStringDate();
+            UserCanDeleteMaintenanceRecord = new MaintenanceRecordManageFeature().HasPermission(currentPerson, FieldVisit.MaintenanceRecord).HasPermission;
+            SortedMaintenanceRecordObservations = FieldVisit.MaintenanceRecord.MaintenanceRecordObservations.ToList()
+                .OrderBy(x => x.TreatmentBMPTypeCustomAttributeType.SortOrder)
+                .ThenBy(x => x.TreatmentBMPTypeCustomAttributeType.GetDisplayName());
+            UserHasCustomAttributeTypeManagePermissions = new NeptuneAdminFeature().HasPermissionByPerson(currentPerson);
         }
+
     }
 }
