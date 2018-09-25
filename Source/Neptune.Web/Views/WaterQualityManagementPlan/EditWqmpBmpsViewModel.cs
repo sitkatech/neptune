1829037ca9aeb5f94d1ec8d254ddbee341e0d08e
jenna mccarter <jenna.mccarter@sitkatech.com>
2018-09-25 16:11:07 -0700

    [neptune/#168] Add structural and source control BMPs to WQMP
    * Add validation if there are multiple Quick BMPs with the same name

--- Source/Neptune.Web/Views/WaterQualityManagementPlan/EditWqmpBmpsViewModel.cs
+++ Source/Neptune.Web/Views/WaterQualityManagementPlan/EditWqmpBmpsViewModel.cs
@@ -81,10 +81,17 @@ namespace Neptune.Web.Views.WaterQualityManagementPlan
 
             var quickBMPNoteMaxCharacterLength = QuickBMP.FieldLengths.QuickBMPNote;
             var sourceControlBMPNoteMaxCharacterLength = SourceControlBMP.FieldLengths.SourceControlBMPNote;
-            
+            var quickBmpWithDuplicateNames = QuickBmpSimples.Duplicates(x => x.DisplayName).Select(x => x.DisplayName).Distinct();
+
+
+            foreach (var quickBmpWithDuplicateName in quickBmpWithDuplicateNames)
+            {
+                validationResults.Add(new ValidationResult($"\"{quickBmpWithDuplicateName}\" has already been used Under Other Strucural BMPs. Make sure that all names are unique."));
+            }
+
             foreach (var quickBMPSimple in QuickBmpSimples ?? new List<QuickBMPSimple>())
             {
-                var quickBMPNoteCharacterLength = quickBMPSimple?.QuickBMPNote.Length;
+                var quickBMPNoteCharacterLength = quickBMPSimple?.QuickBMPNote?.Length;
                 if (quickBMPNoteCharacterLength != null && quickBMPNoteCharacterLength > quickBMPNoteMaxCharacterLength)
                 {
                     validationResults.Add(new ValidationResult($"\"{quickBMPSimple?.DisplayName}\"'s note is too long. Notes have a maximum of {quickBMPNoteMaxCharacterLength} characters and is {quickBMPNoteCharacterLength - quickBMPNoteMaxCharacterLength} over the limit."));
