006046a742eecc6b0fec4190c03e4210f15bda79
jenna mccarter <jenna.mccarter@sitkatech.com>
2018-10-08 14:31:56 -0700

    [neptune/#174] Track WQMP Record Number and Acreage
    * Sql update

--- Database/ReleaseScript/088 - Add RecordNumber and RecordedWQMPAreaInAcres column to WaterQualityManagementPlan table.sql
+++ Database/ReleaseScript/088 - Add RecordNumber and RecordedWQMPAreaInAcres column to WaterQualityManagementPlan table.sql
@@ -6,6 +6,7 @@ RecordedWQMPAreaInAcres DECIMAL(5,1) NULL;
 
 ALTER TABLE dbo.Parcel
 ADD ParcelAreaInAcres float;
+GO
 
 UPDATE dbo.Parcel SET ParcelAreaInAcres = ParcelGeometry.STArea() * 0.0002471053821147119
 
