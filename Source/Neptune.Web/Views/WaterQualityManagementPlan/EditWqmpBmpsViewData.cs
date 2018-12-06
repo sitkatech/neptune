18e3b365d1a9cebb835ad622707a822f0f93d296
Nick <nick.padinha@sitkatech.com>
2018-12-06 10:36:11 -0800

    Neptune bugfix -- the edit wqmp bmps page was trying to load a detail url with the wrong ID parameter causing null exceptions

--- Source/Neptune.Web/Views/WaterQualityManagementPlan/EditWqmpBmpsViewData.cs
+++ Source/Neptune.Web/Views/WaterQualityManagementPlan/EditWqmpBmpsViewData.cs
@@ -18,7 +18,7 @@ namespace Neptune.Web.Views.WaterQualityManagementPlan
             EntityName = $"{Models.FieldDefinition.WaterQualityManagementPlan.GetFieldDefinitionLabelPluralized()}";
             EntityUrl = SitkaRoute<WaterQualityManagementPlanController>.BuildUrlFromExpression(x => x.Index());
             SubEntityName = waterQualityManagementPlan.WaterQualityManagementPlanName;
-            SubEntityUrl = SitkaRoute<WaterQualityManagementPlanController>.BuildUrlFromExpression(x => x.Detail(waterQualityManagementPlan.WaterQualityManagementPlanPriorityID));
+            SubEntityUrl = SitkaRoute<WaterQualityManagementPlanController>.BuildUrlFromExpression(x => x.Detail(waterQualityManagementPlan.WaterQualityManagementPlanID));
             PageTitle = "Edit WQMP BMPs";
 
 
