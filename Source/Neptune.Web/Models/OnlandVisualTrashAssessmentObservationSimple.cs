159e84cc0ceb8fbbe2c6ecacfb2c974cd4508a76
Nick <nick.padinha@sitkatech.com>
2019-01-16 14:04:12 -0800

    [neptune/#214] Step 3: Record observations for an OVTA Add OVTAOSimple for roundtripping

---
+++ Source/Neptune.Web/Models/OnlandVisualTrashAssessmentObservationSimple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Neptune.Web.Models
+{
+    public class OnlandVisualTrashAssessmentObservationSimple
+    {
+        public int OnlandVisualTrashAssessmentObservationID { get; set; }
+        public int OnlandVisualTrashAssessmentID { get; set; }
+        public string Note { get; set; }
+        public DateTime ObservationDatetime { get; set; }
+        public decimal? LocationX { get; set; }
+        public decimal? LocationY { get; set; }
+
+        /// <summary>
+        /// Needed by ModelBinder
+        /// </summary>
+        public OnlandVisualTrashAssessmentObservationSimple()
+        {
+
+        }
+
+        public OnlandVisualTrashAssessmentObservationSimple(OnlandVisualTrashAssessmentObservation o)
+        {
+            OnlandVisualTrashAssessmentObservationID = o.OnlandVisualTrashAssessmentObservationID;
+            OnlandVisualTrashAssessmentID = o.OnlandVisualTrashAssessmentID;
+            Note = o.Note;
+            ObservationDatetime = o.ObservationDatetime;
+            LocationX = o.LocationPoint.XCoordinate.HasValue ? (decimal?) o.LocationPoint.XCoordinate : null;
+            LocationY = o.LocationPoint.YCoordinate.HasValue ? (decimal?) o.LocationPoint.YCoordinate : null;
+        }
+    }
+}
