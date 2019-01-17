e21b3eb96880b8512ca2bf0dc4f5f5a2457c267e
Nick <nick.padinha@sitkatech.com>
2019-01-17 09:06:03 -0800

    [neptune/#214] Step 3: Record observations for an OVTA Display an angular-driven map with a default map extent using a gutted copy of the TMM controller on the Record Observations page.

---
+++ Source/Neptune.Web/Areas/Trash/Views/OnlandVisualTrashAssessment/OVTAObservationsMapInitJson.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Neptune.Web.Models;
+
+namespace Neptune.Web.Areas.Trash.Views.OnlandVisualTrashAssessment
+{
+    public class OVTAObservationsMapInitJson : MapInitJson
+    {
+
+        public OVTAObservationsMapInitJson(string mapDivID)
+            : base(mapDivID, DefaultZoomLevel, MapInitJsonHelpers.GetJurisdictionMapLayers().ToList(),
+                BoundingBox.MakeNewDefaultBoundingBox())
+        {
+        }
+
+    }
+}
