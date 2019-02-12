8ddaa4eab7045d24efec541df7302850c5ef7f9f
Nick <nick.padinha@sitkatech.com>
2019-02-12 09:06:58 -0800

    Neptune Bug fix: Applied changes from previous commit to Modeling and Trash areas. Refactor: moved the floating ribbon javascript to a separate .js file, linked it in NeptuneIncludes, and removed it from all NavAndHeaderLayout.cshtml files

---
+++ Source/Neptune.Web/ScriptsCustom/NeptuneFloatingRibbon.es5.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+jQuery(function () {
+    jQuery(window).scroll(function () {
+        // this is fragile, the 10px buffer is for when the document height is just equal to the height of the view port + the height of the header
+        var outerHeight = jQuery(document).height();
+        var heightOfViewPort = jQuery(window).height();
+        var headerHeight = jQuery('.neptuneHeader')[0].offsetHeight;
+
+        if (jQuery(window).scrollTop() > headerHeight && outerHeight > heightOfViewPort + headerHeight + 10) {
+            jQuery('.neptuneNavbar').addClass('navbar-fixed-top');
+            jQuery("#mainnavandcontent").css("margin-top", jQuery("#neptune-collapse-navbar").css("height"));
+        }
+        if (jQuery(window).scrollTop() <= headerHeight) {
+            jQuery('.neptuneNavbar').removeClass('navbar-fixed-top');
+            jQuery("#mainnavandcontent").css("margin-top", "0");
+        }
+    });
+});
+
