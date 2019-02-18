37073dde71058aaec3b537020008d8946a28d774
Nick <nick.padinha@sitkatech.com>
2019-02-18 14:35:36 -0800

    Neptune Eager load a couple additional tables on the Treatment BMP grid

--- Source/Neptune.Web/Controllers/TreatmentBMPController.cs
+++ Source/Neptune.Web/Controllers/TreatmentBMPController.cs
@@ -80,7 +80,10 @@ namespace Neptune.Web.Controllers
             var showDelete = new JurisdictionManageFeature().HasPermissionByPerson(currentPerson);
             var showEdit = new JurisdictionEditFeature().HasPermissionByPerson(currentPerson);
             gridSpec = new TreatmentBMPGridSpec(currentPerson, showDelete, showEdit);
-            return HttpRequestStorage.DatabaseEntities.TreatmentBMPs.Include(x => x.TreatmentBMPType).Include(x => x.TreatmentBMPAssessments).Include(x => x.WaterQualityManagementPlan).ToList().Where(x => x.CanView(CurrentPerson)).ToList();
+            return HttpRequestStorage.DatabaseEntities.TreatmentBMPs.Include(x => x.StormwaterJurisdiction)
+                .Include(x => x.MaintenanceRecords).Include(x => x.TreatmentBMPBenchmarkAndThresholds)
+                .Include(x => x.TreatmentBMPType).Include(x => x.TreatmentBMPAssessments)
+                .Include(x => x.WaterQualityManagementPlan).ToList().Where(x => x.CanView(CurrentPerson)).ToList();
         }
 
         [TreatmentBMPViewFeature]
