0869b8b6d76981fbe0a11d50694a43636ef353bc
Nick <nick.padinha@sitkatech.com>
2019-02-21 15:20:21 -0800

    [neptune/#212] Display OVTA layer on Trash module home page Map Use the cached score instead of recalculating for the geoserver view

--- Database/Objects/Views/dbo.vGeoServerOnlandVisualTrashAssessmentArea.sql
+++ Database/Objects/Views/dbo.vGeoServerOnlandVisualTrashAssessmentArea.sql
@@ -1,9 +1,7 @@
 create view dbo.vGeoServerOnlandVisualTrashAssessmentArea
-as
-select area.*, aggscore.Score from ( select area.OnlandVisualTrashAssessmentAreaID, cast(round(avg(cast (score.NumericValue as decimal (5,4))), 0) as int) as Score
- from OnlandVisualTrashAssessment ovta
-	join OnlandVisualTrashAssessmentArea area on ovta.OnlandVisualTrashAssessmentAreaID = area.OnlandVisualTrashAssessmentAreaID
-	join OnlandVisualTrashAssessmentScore score on ovta.OnlandVisualTrashAssessmentScoreID = score.OnlandVisualTrashAssessmentScoreID
-	where ovta.OnlandVisualTrashAssessmentStatusID = 2
-	group by area.OnlandVisualTrashAssessmentAreaID ) aggscore join OnlandVisualTrashAssessmentArea area on area.OnlandVisualTrashAssessmentAreaID = aggscore.OnlandVisualTrashAssessmentAreaID
-
+as select area.OnlandVisualTrashAssessmentAreaID,
+area.OnlandVisualTrashAssessmentAreaName,
+area.StormwaterJurisdictionID,
+area.OnlandVisualTrashAssessmentAreaGeometry,
+area.OnlandVisualTrashAssessmentScoreID as Score
+from dbo.OnlandVisualTrashAssessmentArea area join dbo.OnlandVisualTrashAssessmentScore score on area.OnlandVisualTrashAssessmentScoreID = score.OnlandVisualTrashAssessmentScoreID
