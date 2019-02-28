79512ed686be4f6cd7f5a79b39c618d5f743308b
Nick <nick.padinha@sitkatech.com>
2019-02-28 10:25:41 -0800

    [neptune/#266] Initial Delineation Workflow Map update vGeoSwerverParcel

--- Database/Objects/Views/dbo.vGeoServerParcel.sql
+++ Database/Objects/Views/dbo.vGeoServerParcel.sql
@@ -5,14 +5,14 @@ go
 create view dbo.vGeoServerParcel
 as
 select
-	ParcelID,
-	ParcelNumber,
-	ParcelGeometry,
-	OwnerName,
-	ParcelStreetNumber,
-	ParcelAddress,
-	ParcelZipCode,
-	LandUse,
-	isnull(ParcelAreaInAcres, 0) as ParcelArea 
-
-from dbo.Parcel
+	p.ParcelID,
+	p.ParcelNumber,
+	p.ParcelGeometry,
+	p.OwnerName,
+	p.ParcelStreetNumber,
+	p.ParcelAddress,
+	p.ParcelZipCode,
+	p.LandUse,
+	isnull(p.ParcelAreaInAcres, 0) as ParcelArea,
+	cast(case when w.ParcelID is null then 0 else 1 end as bit) as IsWQMPParcel
+from dbo.Parcel p left join (select distinct parcelid from dbo.WaterQualityManagementPlanParcel) w on p.ParcelID = w.ParcelID
