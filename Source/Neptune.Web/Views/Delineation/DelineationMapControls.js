350af94d71b97e09becb78cc3041b553ec43179c
Nick <nick.padinha@sitkatech.com>
2019-03-11 10:23:15 -0700

    [neptune/#227] Distributed BMP Delineation - Draw and Revise Catchment Refactor BeginDelineationControl to use the template-driven pattern

--- Source/Neptune.Web/Views/Delineation/DelineationMapControls.js
+++ Source/Neptune.Web/Views/Delineation/DelineationMapControls.js
@@ -1,6 +1,7 @@
 ﻿// page-specific leaflet controls.
 // todo: the use of window.delineationMap throughout to back-reference the map object is a little brittle
 
+// WIP: base class for the html-template driven control pattern
 L.Control.TemplatedControl = L.Control.extend({
     templateID: null,
 
@@ -19,6 +20,10 @@ L.Control.TemplatedControl = L.Control.extend({
 
         this.initializeControlInstance(map);
         return this.parentElement;
+    },
+
+    onRemove: function(map) {
+        jQuery(this.parentElement).remove();
     }
 });
 
@@ -37,7 +42,7 @@ L.Control.Watermark = L.Control.extend({
     },
 
     onRemove: function (map) {
-        // Nothing to do here
+        jQuery(this.parentElement).remove();
     }
 });
 
@@ -165,72 +170,23 @@ L.control.delineationSelectedAsset = function (opts) {
 
 L.Control.BeginDelineation = L.Control.extend({
     onAdd: function (map) {
-        // todo: is there a cleaner way to generate this stuff than L.DomUtil
-        this._div = L.DomUtil.create("div", "beginDelineationControl leaflet-bar");
-        stopClickPropagation(this._div);
 
-        var titleBar = L.DomUtil.create("div", "row");
+        var t = document.querySelector("#beginDelineationControlTemplate");
+        this.parentElement = document.importNode(t.content, true).firstElementChild;
 
-        var title = L.DomUtil.create("div", "col-sm-10");
-        title.innerHTML = "<h4>Delineate Drainage Area</h4>";
-
-        var closeButtonWrapper = L.DomUtil.create("div", "col-sm-2 text-right");
-
-        var closeButton = L.DomUtil.create("button", "btn btn-sm btn-neptune");
-        closeButton.innerHTML = "x";
-        L.DomEvent.on(closeButton,
-            "click",
-            function (e) {
-                window.delineationMap.removeBeginDelineationControl();
-                e.stopPropagation();
-            });
-
-        closeButtonWrapper.append(closeButton);
-        titleBar.append(title);
-        titleBar.append(closeButtonWrapper);
-
-        this._div.append(titleBar);
-
-        var main = L.DomUtil.create("div", "beginDelineationOptions");
-        // todo: add values to these radios when we start building out this step
-        main.innerHTML = "<label>1. Select the type of flow this BMP will receive</label><br/>" +
-            "<label class='group'><input type='radio' name='typeOfFlow'> Receives local surface flow only</label><br/>" +
-            "<label class='group'><input type='radio' name='typeOfFlow'> Receives piped flow only</label><br/>" +
-            "<label class='group'><input type='radio' name='typeOfFlow'> Receives both piped flow and surface flow</label><hr/>" +
-            "<label>2. Choose a delineation option</label></br>" +
-            "<label class='group'><input type='radio' name='delineationOption'> Delineate Automatically from DEM</label><br/>" +
-
-            "<label class='group'><input type='radio' name='delineationOption'> Draw the Catchment Area</label><br/>" +
-            "<label class='group'><input type='radio' name='delineationOption'> Upload a GIS file</label></hr>" +
-            //todo get rid of this
-            "<p>(Content under development)</p>";
-
-        this._div.append(main);
-
-        // todo: add a handler to goBtn and undisable it
-        var formBtnWrapper = L.DomUtil.create("div", "text-right");
-        var goBtn = L.DomUtil.create("button", "continueDelineate btn btn-sm btn-neptune");
-        goBtn.type = "button";
-        goBtn.innerHTML = "Delineate";
-        goBtn.disabled = "disabled";
-        var stopBtn = L.DomUtil.create("button", "cancelDelineate btn btn-sm btn-neptune");
-        stopBtn.type = "button";
-        stopBtn.innerHTML = "Cancel";
+        stopClickPropagation(this.parentElement);
+        var stopBtn = this.parentElement.querySelector("#cancelDelineationButton");
         L.DomEvent.on(stopBtn,
             "click",
             function (e) {
                 window.delineationMap.removeBeginDelineationControl();
                 e.stopPropagation();
             });
-        formBtnWrapper.append(goBtn);
-        formBtnWrapper.append(stopBtn);
 
-        this._div.append(formBtnWrapper);
-
-
-        return this._div;
+        return this.parentElement;
     },
     onRemove: function () {
+        jQuery(this.parentElement).remove();
     }
 });
 
