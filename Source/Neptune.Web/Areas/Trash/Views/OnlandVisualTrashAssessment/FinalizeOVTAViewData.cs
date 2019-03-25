8d2985982fc0d17b48b729ba7a58ea1e68e5a4bd
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-03-25 13:15:22 -0700

    [neptune/#222] Display visit details on child OVTA detail page Move Observation Summary Map to partial for maximum code reuse

--- Source/Neptune.Web/Areas/Trash/Views/OnlandVisualTrashAssessment/FinalizeOVTAViewData.cs
+++ Source/Neptune.Web/Areas/Trash/Views/OnlandVisualTrashAssessment/FinalizeOVTAViewData.cs
@@ -11,22 +11,20 @@ namespace Neptune.Web.Areas.Trash.Views.OnlandVisualTrashAssessment
 {
     public class FinalizeOVTAViewData : OVTASectionViewData
     {
-        public OVTASummaryMapInitJson OVTASummaryMapInitJson { get; }
         public IEnumerable<SelectListItem> Scores { get; }
         public Person ConductedByPerson { get; }
         public StormwaterJurisdiction Jurisdiction { get; }
         public string CreatedDate { get; }
-        public string GeoServerUrl { get; }
         public string ScoreDescriptionsUrl { get; }
+
+        public TrashAssessmentSummaryMapViewData TrashAssessmentSummaryMapViewData { get; }
         public IEnumerable<PreliminarySourceIdentificationType> PreliminarySourceIdentificationTypeOthers { get; }
 
         public FinalizeOVTAViewData(Person currentPerson,
             Models.OnlandVisualTrashAssessment ovta, OVTASummaryMapInitJson ovtaSummaryMapInitJson, IEnumerable<SelectListItem> scores, string geoServerUrl)
             : base(currentPerson, Models.OVTASection.FinalizeOVTA, ovta)
         {
-            OVTASummaryMapInitJson = ovtaSummaryMapInitJson;
             Scores = scores;
-            GeoServerUrl = geoServerUrl;
             ConductedByPerson = ovta.CreatedByPerson;
             Jurisdiction = ovta.StormwaterJurisdiction;
             CreatedDate = ovta.CreatedDate.ToShortDateString();
@@ -34,6 +32,8 @@ namespace Neptune.Web.Areas.Trash.Views.OnlandVisualTrashAssessment
                 SitkaRoute<OnlandVisualTrashAssessmentController>.BuildUrlFromExpression(x => x.ScoreDescriptions());
 
             PreliminarySourceIdentificationTypeOthers = PreliminarySourceIdentificationType.All.Where(x=>x.IsOther());
+
+            TrashAssessmentSummaryMapViewData = new TrashAssessmentSummaryMapViewData(ovtaSummaryMapInitJson, ovta.OnlandVisualTrashAssessmentObservations, geoServerUrl);
         }
     }
 }
