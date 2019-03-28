40aba8c85d25b867599b22ae8e4c262d5b82d7b1
Ian Stavros <ian.stavros@sitkatech.com>
2019-03-28 15:18:39 -0700

    Moved Multiple Features into their own .cs file

---
+++ Source/Neptune.Web/Security/OnlandVisualTrashAssessmentAreaDeleteFeature.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Neptune.Web.Models;
+
+namespace Neptune.Web.Security
+{
+    [SecurityFeatureDescription("Allows Viewing an OVTA Area")]
+    public class OnlandVisualTrashAssessmentAreaDeleteFeature : NeptuneFeatureWithContext, INeptuneBaseFeatureWithContext<OnlandVisualTrashAssessmentArea>
+    {
+        private readonly NeptuneFeatureWithContextImpl<OnlandVisualTrashAssessmentArea> _lakeTahoeInfoFeatureWithContextImpl;
+
+        public OnlandVisualTrashAssessmentAreaDeleteFeature()
+            : base(new List<Role> { Role.SitkaAdmin, Role.Admin, Role.JurisdictionManager, Role.JurisdictionEditor})
+        {
+            _lakeTahoeInfoFeatureWithContextImpl = new NeptuneFeatureWithContextImpl<OnlandVisualTrashAssessmentArea>(this);
+            ActionFilter = _lakeTahoeInfoFeatureWithContextImpl;
+        }
+
+        public void DemandPermission(Person person, OnlandVisualTrashAssessmentArea contextModelObject)
+        {
+            _lakeTahoeInfoFeatureWithContextImpl.DemandPermission(person, contextModelObject);
+        }
+
+        public PermissionCheckResult HasPermission(Person person, OnlandVisualTrashAssessmentArea contextModelObject)
+        {
+            var stormwaterJurisdiction = contextModelObject.StormwaterJurisdiction;
+
+            if (stormwaterJurisdiction != null && !person.CanEditStormwaterJurisdiction(stormwaterJurisdiction))
+            {
+                return new PermissionCheckResult($"You do not have permission to view or manage OVTA Areas for {stormwaterJurisdiction.GetOrganizationDisplayName()}");
+            }
+            return new PermissionCheckResult();
+        }
+    }
+}
