884659d7368256095ca64922af1c4d0e32bf36b5
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-04-08 18:06:27 -0700

    Don't exclude unassessed areas from the topological calculations

--- Database/Objects/Views/dbo.vOnlandVisualTrashAssessmentAreaDated.sql
+++ Database/Objects/Views/dbo.vOnlandVisualTrashAssessmentAreaDated.sql
@@ -4,6 +4,5 @@ GO
 Create View dbo.vOnlandVisualTrashAssessmentAreaDated as
 Select a.OnlandVisualTrashAssessmentAreaID, a.OnlandVisualTrashAssessmentAreaGeometry, q.MostRecentAssessmentDate from OnlandVisualTrashAssessmentArea a inner join 
 (Select OnlandVisualTrashAssessmentAreaID, Max(CompletedDate) as MostRecentAssessmentDate From OnlandVisualTrashAssessment
-where OnlandVisualTrashAssessmentStatusID = 2
 group by OnlandVisualTrashAssessmentAreaID) q on a.OnlandVisualTrashAssessmentAreaID = q.OnlandVisualTrashAssessmentAreaID
 go
