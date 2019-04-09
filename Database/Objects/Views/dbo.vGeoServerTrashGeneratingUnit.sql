09f9e8e9232985f439cef3eaf5a919c0d301b1d4
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-04-09 09:12:52 -0700

    Rename OnlandVisualTrashAssessmentAreaID in vGeoServerTrashGeneratingUnit so that ArcGis doesn't freak out

--- Database/Objects/Views/dbo.vGeoServerTrashGeneratingUnit.sql
+++ Database/Objects/Views/dbo.vGeoServerTrashGeneratingUnit.sql
@@ -1,6 +1,13 @@
 Drop view if exists dbo.vGeoServerTrashGeneratingUnit
 Go
 
-Create view dbo.vGeoServerTrashGeneratingUnit
-as select * from dbo.TrashGeneratingUnit
+Create view dbo.vGeoServerTrashGeneratingUnit as
+select
+	TrashGeneratingUnitID,
+	StormwaterJurisdictionID,
+	TreatmentBMPID,
+	OnlandVisualTrashAssessmentAreaID as AssessmentAreaID,
+	LandUseBlockID,
+	TrashGeneratingUnitGeometry
+from dbo.TrashGeneratingUnit
 Go
