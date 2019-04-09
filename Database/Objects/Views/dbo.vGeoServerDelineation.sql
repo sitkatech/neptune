a62cadcf09bdc7c87b73029d6b0e61cc8d11fcd1
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-04-09 15:18:04 -0700

    [neptune/#323] Display delineations as tile layer on Delineation Map Create view for GeoServer

---
+++ Database/Objects/Views/dbo.vGeoServerDelineation.sql
@@ -0,0 +1,14 @@
+Drop View If Exists dbo.vGeoServerDelineation
+GO
+
+Create View dbo.vGeoServerDelineation as
+Select
+	d.DelineationID,
+	DelineationGeometry,
+	DelineationTypeName as DelineationType,
+	TreatmentBMPID
+from
+	dbo.Delineation d inner join dbo.DelineationType dt
+		on d.DelineationTypeID = dt.DelineationTypeID
+	inner join dbo.TreatmentBMP t
+		on d.DelineationID = t.DelineationID
