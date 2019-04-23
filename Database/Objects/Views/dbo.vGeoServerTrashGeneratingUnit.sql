e8db90e3cd4c9cc72f54bfcfc704492ffc1c047d
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-04-23 10:22:32 -0700

    [neptune/#297] Display TGUs on Area-Based and Load-Based results maps Fix TGU style so it shows untreated TGUs

--- Database/Objects/Views/dbo.vGeoServerTrashGeneratingUnit.sql
+++ Database/Objects/Views/dbo.vGeoServerTrashGeneratingUnit.sql
@@ -7,7 +7,7 @@ select
 	tgu.StormwaterJurisdictionID,
 	tcs.TrashCaptureStatusTypeName as TrashCaptureStatus,
 	ovtaad.MostRecentAssessmentScore as AssessmentScore,
-	Case When tgu.LandUseBlockID is null then 0 else 1 end as IsPriorityLandUse,
+	Case When lub.PriorityLandUseTypeID is null then 0 else 1 end as IsPriorityLandUse,
 	TrashGeneratingUnitGeometry
 from dbo.TrashGeneratingUnit tgu
 	left join dbo.vOnlandVisualTrashAssessmentAreaDated ovtaad
@@ -16,4 +16,6 @@ from dbo.TrashGeneratingUnit tgu
 		on tgu.TreatmentBMPID = tbmp.TreatmentBMPID
 	join dbo.TrashCaptureStatusType tcs
 		on tcs.TrashCaptureStatusTypeID = tbmp.TrashCaptureStatusTypeID
+	left join dbo.LandUseBlock lub
+		on lub.LandUseBlockID = tgu.LandUseBlockID
 Go
