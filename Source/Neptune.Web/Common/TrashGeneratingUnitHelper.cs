bc0c28134fdb038eb59d866f53b8080a1096c4d3
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-04-29 16:13:39 -0700

    [neptune/#326] Make TGU Layer refresh periodically Wrap update call for Delineation

---
+++ Source/Neptune.Web/Common/TrashGeneratingUnitHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Neptune.Web.Models;
+
+namespace Neptune.Web.Common
+{
+    public static class TrashGeneratingUnitHelper
+    {
+        public const string DelineationObjectType = "Delineation";
+        public const string OnlandVisualTrashAssessmentAreaObjectType = "OnlandVisualTrashAssessmentArea";
+
+        public static void UpdateTrashGeneratingUnits(this Delineation delineation)
+        {
+            var objectIDs = new SqlParameter("@ObjectIDs", FormatIDString(new List<int> {delineation.DelineationID}));
+            var objectType = new SqlParameter("@ObjectType", DelineationObjectType);
+
+            HttpRequestStorage.DatabaseEntities.Database.ExecuteSqlCommand(
+                "dbo.pRebuildTrashGeneratingUnitTableRelative @ObjectIDs, @ObjectType", objectIDs, objectType);
+        }
+
+        public static string FormatIDString(IEnumerable<int> idList)
+        {
+            return string.Join(",", idList);
+        }
+    }
+}
