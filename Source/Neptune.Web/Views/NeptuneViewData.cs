2d3f2b76949cc3a5bef403479d389c5ac50781fe
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-05-06 13:37:53 -0700

    Remove Delineation Map from Program Overview menu

--- Source/Neptune.Web/Views/NeptuneViewData.cs
+++ Source/Neptune.Web/Views/NeptuneViewData.cs
@@ -130,8 +130,6 @@ namespace Neptune.Web.Views
             programInfoMenu.AddMenuItem(LtInfoMenuItem.MakeItem(new SitkaRoute<TreatmentBMPTypeController>(c => c.Index()), currentPerson, "Treatment BMP Types", "Group1"));
             programInfoMenu.AddMenuItem(LtInfoMenuItem.MakeItem(new SitkaRoute<FundingSourceController>(c => c.Index()), currentPerson, Models.FieldDefinition.FundingSource.GetFieldDefinitionLabelPluralized(), "Group1"));
 
-            programInfoMenu.AddMenuItem(LtInfoMenuItem.MakeItem(new SitkaRoute<DelineationController>(c => c.DelineationMap(null)), currentPerson, "Delineation Map", "Group1"));
-
             return programInfoMenu;
         }
 
