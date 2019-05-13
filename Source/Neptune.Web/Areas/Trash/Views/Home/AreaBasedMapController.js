1d779cd89d11753ddbd484dbc528caa7aeab1577
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-05-13 12:53:24 -0700

    Put back the onMapClick function that got overwritten by the merge

--- Source/Neptune.Web/Areas/Trash/Views/Home/AreaBasedMapController.js
+++ Source/Neptune.Web/Areas/Trash/Views/Home/AreaBasedMapController.js
@@ -24,4 +24,43 @@
         trashMapService.saveBounds($scope.neptuneMap.map.getBounds());
         trashMapService.saveCenter($scope.neptuneMap.map.getCenter());
         trashMapService.saveStormwaterJurisdictionID(resultsControl.getSelectedJurisdictionID());
+
+        function onMapClick(event) {
+            var layerName = "OCStormwater:TrashGeneratingUnits";
+            var mapServiceUrl = $scope.AngularViewData.MapServiceUrl;
+            if (!layerName || !mapServiceUrl) {
+                return;
+            }
+
+            debugger;
+
+            var latlng = event.latLng;
+            var latLngWrapped = latlng.wrap();
+            var parameters = L.Util.extend($scope.neptuneMap.createWfsParamsWithLayerName(layerName),
+                {
+                    typeName: layerName,
+                    cql_filter: "intersects(ParcelGeometry, POINT(" + latLngWrapped.lat + " " + latLngWrapped.lng + "))"
+                });
+            jQuery.ajax({
+                    url: mapServiceUrl + L.Util.getParamString(parameters),
+                    dataTpe: "json",
+                    jsonCallback: "getJson"
+                },
+                function (response) {
+                    if (response.features.length == 0) {
+                        return;
+                    }
+                    var mergedProperties = _.merge.apply(_, _.map(response.features, "properties"));
+                    $scope.toggle(mergedProperties.JurisdictionID,
+                        function () {
+                            $scope.$apply();
+                        });
+                },
+                function () {
+                    console.error("There was an error selecting the " +
+                        $scope.AngularViewData.JurisdictionID +
+                        "from list");
+                });
+        }
+
     });
