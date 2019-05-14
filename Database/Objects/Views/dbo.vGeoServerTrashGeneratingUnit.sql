0973849bf1a5b0b9bf6b71fc91f30b9c274a56cd
Ian Stavros <ian.stavros@sitkatech.com>
2019-05-14 09:46:27 -0700

    Updated Geoserver Layer Properties
    added TreatmentBMPID, TreatmentBMPName, PriorityLandUseTypeDisplayName as LandUseType, LastUpdateDate as LastCalculatedDate, OrganizationName, OrganizationID to geoServeTrashGeneratingUnit geoLayer

--- Database/Objects/Views/dbo.vGeoServerTrashGeneratingUnit.sql
+++ Database/Objects/Views/dbo.vGeoServerTrashGeneratingUnit.sql
@@ -5,13 +5,18 @@ Create view dbo.vGeoServerTrashGeneratingUnit as
 select
 	TrashGeneratingUnitID,
 	tgu.StormwaterJurisdictionID,
+	o.OrganizationID,
+	o.OrganizationName,
 	case when tbmp.TreatmentBMPID is null then 'NotProvided' else tcs.TrashCaptureStatusTypeName end as TrashCaptureStatus,
 	case when ovtaad.MostRecentAssessmentScore is null then 'NotProvided' else ovtaad.MostRecentAssessmentScore end as AssessmentScore,
 	Case When lub.PriorityLandUseTypeID is null then 0 else 1 end as IsPriorityLandUse,
 	Case when tgu.LandUseBlockID is null then 1 else 0 end as NoDataProvided,
 	TrashGeneratingUnitGeometry,
 	ovtaad.OnlandVisualTrashAssessmentAreaID,
-	tbmp.TreatmentBMPID
+	tbmp.TreatmentBMPID,
+	tbmp.TreatmentBMPName,
+	plut.PriorityLandUseTypeDisplayName as LandUseType,
+	tgu.LastUpdateDate as LastCalculatedDate
 from dbo.TrashGeneratingUnit tgu
 	left join dbo.vOnlandVisualTrashAssessmentAreaDated ovtaad
 		on tgu.OnlandVisualTrashAssessmentAreaID = ovtaad.OnlandVisualTrashAssessmentAreaID
@@ -21,4 +26,10 @@ from dbo.TrashGeneratingUnit tgu
 		on tcs.TrashCaptureStatusTypeID = tbmp.TrashCaptureStatusTypeID
 	left join dbo.LandUseBlock lub
 		on lub.LandUseBlockID = tgu.LandUseBlockID
+	left join dbo.PriorityLandUseType plut
+		on lub.PriorityLandUseTypeID = plut.PriorityLandUseTypeID
+	left join dbo.StormwaterJurisdiction sj
+		on tgu.StormwaterJurisdictionID = sj.StormwaterJurisdictionID
+	left join dbo.Organization o
+		on o.OrganizationID = sj.OrganizationID
 Go
