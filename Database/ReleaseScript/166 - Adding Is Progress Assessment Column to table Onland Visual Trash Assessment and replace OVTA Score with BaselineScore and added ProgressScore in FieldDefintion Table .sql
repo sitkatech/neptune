f1f9f90a966ee5b19957b0f7e0a0b76f7c815f69
Ian Stavros <ian.stavros@sitkatech.com>
2019-05-14 17:11:19 -0700

    Relabled Script 165 to 166 to avoid duplicate release scripts

--- Database/ReleaseScript/166 - Adding Is Progress Assessment Column to table Onland Visual Trash Assessment and replace OVTA Score with BaselineScore and added ProgressScore in FieldDefintion Table .sql
+++ Database/ReleaseScript/166 - Adding Is Progress Assessment Column to table Onland Visual Trash Assessment and replace OVTA Score with BaselineScore and added ProgressScore in FieldDefintion Table .sql
@@ -9,3 +9,12 @@ GO
 
 Alter Table dbo.OnlandVisualTrashAssessment
 Alter Column IsProgressAssessment Bit Not Null
+
+GO
+
+Update dbo.FieldDefinition Set FieldDefinitionName = 'BaselineScore', FieldDefinitionDisplayName = 'Baseline Score' Where FieldDefinitionName = 'OVTAScore'
+
+GO
+
+Insert Into dbo.FieldDefinition (FieldDefinitionID, FieldDefinitionName, FieldDefinitionDisplayName, DefaultDefinition, CanCustomizeLabel)
+Values (63, 'ProgressScore', 'Progress Score', '', 1)
