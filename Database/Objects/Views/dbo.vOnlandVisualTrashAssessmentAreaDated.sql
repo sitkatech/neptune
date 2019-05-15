0897bb329800c34163c4fa5dbbbaf5c7a6cb5431
Ian Stavros <ian.stavros@sitkatech.com>
2019-05-15 11:53:48 -0700

    Adding ProgressScore and BaselineScore to OVTA Area

--- Database/Objects/Views/dbo.vOnlandVisualTrashAssessmentAreaDated.sql
+++ Database/Objects/Views/dbo.vOnlandVisualTrashAssessmentAreaDated.sql
@@ -10,14 +10,15 @@ Select
 	Score.OnlandVisualTrashAssessmentScoreDisplayName as MostRecentAssessmentScore
 from dbo.OnlandVisualTrashAssessmentArea a
 	inner join (
-	Select
-		ovta.OnlandVisualTrashAssessmentID,
-		ovta.OnlandVisualTrashAssessmentAreaID,
-		ovta.OnlandVisualTrashAssessmentScoreID,
-		ovta.CompletedDate,
-		rownumber = Row_Number() over (partition by ovta.OnlandVisualTrashAssessmentAreaID order by ovta.CompletedDate desc)
-	from dbo.OnlandVisualTrashAssessment ovta
-	where CompletedDate is not null) q
+		Select
+			ovta.OnlandVisualTrashAssessmentID,
+			ovta.OnlandVisualTrashAssessmentAreaID,
+			ovta.OnlandVisualTrashAssessmentScoreID,
+			ovta.CompletedDate,
+			rownumber = Row_Number() over (partition by ovta.OnlandVisualTrashAssessmentAreaID order by ovta.CompletedDate desc)
+		from dbo.OnlandVisualTrashAssessment ovta
+		where CompletedDate is not null and IsProgressAssessment = 1
+	) q -- in the future we'll need to find a way to account for Baseline scores as well, but for now we'll only use Progress for results
 		on a.OnlandVisualTrashAssessmentAreaID = q.OnlandVisualTrashAssessmentAreaID
 	join  dbo.OnlandVisualTrashAssessmentScore score
 		on score.OnlandVisualTrashAssessmentScoreID = q.OnlandVisualTrashAssessmentScoreID
