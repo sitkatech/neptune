a69feda22ad7318bcd871c9870f4bc3dc6b8d9b3
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-05-16 12:06:32 -0700

    Create a background job class for the main sequence tgu update

---
+++ Source/Neptune.Web/ScheduledJobs/TrashGeneratingUnitRefreshScheduledBackgroundJob.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Neptune.Web.Common;
+
+namespace Neptune.Web.ScheduledJobs
+{
+    public class TrashGeneratingUnitRefreshScheduledBackgroundJob : ScheduledBackgroundJobBase
+    {
+        public override List<NeptuneEnvironmentType> RunEnvironments => new List<NeptuneEnvironmentType>
+        {
+            NeptuneEnvironmentType.Local,
+            NeptuneEnvironmentType.Prod,
+            NeptuneEnvironmentType.Qa
+        };
+
+        protected override void RunJobImplementation()
+        {
+            TrashGeneratingUnitRefreshImpl();
+        }
+
+        protected virtual void TrashGeneratingUnitRefreshImpl()
+        {
+            Logger.Info($"Processing '{JobName}'");
+
+            HttpRequestStorage.DatabaseEntities.Database.CommandTimeout = 36000;
+            HttpRequestStorage.DatabaseEntities.Database.ExecuteSqlCommand("execute dbo.pRebuildTrashGeneratingUnitTable");
+        }
+    }
+}
