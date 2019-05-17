d7ad56789877a6b55c2c2f5ac22b161a4e9ac3c7
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-05-16 17:00:34 -0700

    rename server

--- Source/Neptune.Web/ScheduledJobs/ScheduledBackgroundJobBootstrapper.cs
+++ Source/Neptune.Web/ScheduledJobs/ScheduledBackgroundJobBootstrapper.cs
@@ -38,11 +38,12 @@ namespace Neptune.Web.ScheduledJobs
             };
             GlobalConfiguration.Configuration.UseSqlServerStorage(NeptuneWebConfiguration.DatabaseConnectionString,
                 sqlServerStorageOptions);
+
             app.UseHangfireServer(new BackgroundJobServerOptions
             {
                 WorkerCount = 1,
-                Queues = new[] { "critical", "default" },
-                ServerName = "Neptune:1337"
+                // 5/16/2019 NP - this is fine.
+                ServerName = "blahblah", 
             }); // 11/03/2015 MF - limit the number of worker threads, we really don't need that many - in fact we try to have each job run serially for the time being because we're not sure how concurrent the different jobs could really be.
 
             // Hangfire defaults to 10 retries for failed jobs; this disables that behavior by doing no automatic retries.
