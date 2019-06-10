7b43f5e6d5e2834f51fa59a64234db87db4f4c54
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-10 08:38:53 -0700

    Add assessment summary to bmp inventory nav

--- Source/Neptune.Web/Controllers/TreatmentBMPController.cs
+++ Source/Neptune.Web/Controllers/TreatmentBMPController.cs
@@ -706,11 +706,3 @@ namespace Neptune.Web.Controllers
         }
     }
 }
-namespace Neptune.Web.Models { 
-public class TreatmentBMPAssessmentSummary
-    {
-        public vMostRecentTreatmentBMPAssessment AssessmentSummary { get; set; }
-
-        public string Notes { get; set; }
-    }
-}
