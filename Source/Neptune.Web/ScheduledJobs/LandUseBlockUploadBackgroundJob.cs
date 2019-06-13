ceb2eabea998f05c02f2190b26e3548e51ee115e
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-13 11:05:45 -0700

    Stub out the methods for the background job

---
+++ Source/Neptune.Web/ScheduledJobs/LandUseBlockUploadBackgroundJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Neptune.Web.Common;
+
+namespace Neptune.Web.ScheduledJobs
+{
+    public class LandUseBlockUploadBackgroundJob : ScheduledBackgroundJobBase
+    {
+        public override List<NeptuneEnvironmentType> RunEnvironments => new List<NeptuneEnvironmentType>
+        {
+            NeptuneEnvironmentType.Local,
+            NeptuneEnvironmentType.Prod,
+            NeptuneEnvironmentType.Qa
+        };
+
+        protected override void RunJobImplementation()
+        {
+            // you can use breakpoints inside hangfire jobs like normal. delete this line of code and comment once you have something real to attach to.
+            var i = 1;
+            //todo: process data from stagging table reporitng errors: on success send successful email, on failue send email of errors in upload file
+            throw new NotImplementedException();
+        }
+    }
+}
--- Source/Neptune.Web/ScheduledJobs/ScheduledBackgroundJobLaunchHelper.cs
+++ Source/Neptune.Web/ScheduledJobs/ScheduledBackgroundJobLaunchHelper.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Neptune.Web.Common;
 using Neptune.Web.Models;
 
 namespace Neptune.Web.ScheduledJobs
@@ -25,16 +22,6 @@ namespace Neptune.Web.ScheduledJobs
         {
             var landUseBlockUploadBackgroundJob = new LandUseBlockUploadBackgroundJob();
             landUseBlockUploadBackgroundJob.RunJob();
-        }
-    }
-
-    public class LandUseBlockUploadBackgroundJob : ScheduledBackgroundJobBase
-    {
-        public override List<NeptuneEnvironmentType> RunEnvironments { get; }
-        protected override void RunJobImplementation()
-        {
-            //todo: process data from stagging table reporitng errors: on success send successful email, on failue send email of errors in upload file
-            throw new NotImplementedException();
         }
     }
 }
