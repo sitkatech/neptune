0ab014f3aa3f8d47bc83db95912455a2c260be2e
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-13 10:04:05 -0700

    Add governing ovta area baseline score to tgu grid

--- Source/Neptune.Web/Areas/Trash/Views/TrashGeneratingUnit/TrashGeneratingUnitGridSpec.cs
+++ Source/Neptune.Web/Areas/Trash/Views/TrashGeneratingUnit/TrashGeneratingUnitGridSpec.cs
@@ -25,6 +25,7 @@ namespace Neptune.Web.Areas.Trash.Views.TrashGeneratingUnit
                 return x.LandUseBlock?.PriorityLandUseType?.PriorityLandUseTypeDisplayName ?? "Not Priority Land Use";
             }, 140, DhtmlxGridColumnFilterType.Text);
             Add("Governing OVTA Area", x => x.OnlandVisualTrashAssessmentArea?.GetDisplayNameAsDetailUrlNoPermissionCheck() ?? new HtmlString(""), 255, DhtmlxGridColumnFilterType.Html);
+            Add("Governing OVTA Area Score", x => x.OnlandVisualTrashAssessmentArea?.GetBaselineScoreAsHtmlString() ?? new HtmlString(""), 255, DhtmlxGridColumnFilterType.Html);
             Add("Governing Treatment BMP", x => x.TreatmentBMP?.GetDisplayNameAsUrl() ?? new HtmlString(""), 190, DhtmlxGridColumnFilterType.Html);
             Add("Stormwater Jurisdiction", x => x.StormwaterJurisdiction?.GetDisplayNameAsDetailUrl() ?? new HtmlString(""), 170, DhtmlxGridColumnFilterType.Html);
             Add("Area", x => ((x.TrashGeneratingUnitGeometry.Area ?? 0) * DbSpatialHelper.SqlGeometryAreaToAcres).ToString("F2", CultureInfo.InvariantCulture), 100, DhtmlxGridColumnFilterType.Numeric);
