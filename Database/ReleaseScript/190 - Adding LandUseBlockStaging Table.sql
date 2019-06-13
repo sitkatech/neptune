c1a5d990537026af6cec00da03179e42358e19a2
Ian Stavros <ian.stavros@sitkatech.com>
2019-06-13 09:33:02 -0700

    Renamed Staging Table

--- Database/ReleaseScript/190 - Adding LandUseBlockStaging Table.sql
+++ Database/ReleaseScript/190 - Adding LandUseBlockStaging Table.sql
@@ -1,7 +1,7 @@
- create table dbo.LandUseBlockGeomteryStaging (
- LandUseBlockStagingID int identity(1,1) not null constraint PK_LandUseBlockStaging_LandUseBlockStagingID primary key,
- PersonID int not null constraint FK_LandUseBlockStaging_Person_PersonID foreign key references dbo.Person (PersonID),
+ create table dbo.LandUseBlockGeometryStaging (
+ LandUseBlockGeometryStagingID int identity(1,1) not null constraint PK_LandUseBlockGeometryStaging_LandUseBlockGeometryStagingID primary key,
+ PersonID int not null constraint FK_LandUseBlockGeometryStaging_Person_PersonID foreign key references dbo.Person (PersonID),
  FeatureClassName varchar(255) not null,
- LandUseBlockStagingGeoJson varchar(max) not null,
+ LandUseBlockGeometryStagingGeoJson varchar(max) not null,
  SelectedProperty varchar(255) null,
  ShouldImport bit not null)
