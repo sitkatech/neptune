b5b5117ecc91f31b496f47d01bd56e5db2188619
Ian Stavros <ian.stavros@sitkatech.com>
2019-06-17 16:55:19 -0700

    uploads to land use block table from land use block staging

--- Source/Neptune.Web/ScheduledJobs/LandUseBlockUploadBackgroundJob.cs
+++ Source/Neptune.Web/ScheduledJobs/LandUseBlockUploadBackgroundJob.cs
@@ -32,7 +32,7 @@ namespace Neptune.Web.ScheduledJobs
             var ogr2OgrCommandLineRunner = new Ogr2OgrCommandLineRunner(NeptuneWebConfiguration.Ogr2OgrExecutable, Ogr2OgrCommandLineRunner.DefaultCoordinateSystemId, NeptuneWebConfiguration.HttpRuntimeExecutionTimeout.TotalMilliseconds*10);
             ogr2OgrCommandLineRunner.ImportGeoJsonToMsSql(
                 landUseBlockGeometryStaging.LandUseBlockGeometryStagingGeoJson,
-                NeptuneWebConfiguration.DatabaseConnectionString, "LandUseBlockStaging", "Select LU_for_TGR as PriorityLandUseType, LU_Descr as LandUseDescription, TGR as TrashGenerationRate, LU_for_TGR as LandUseForTGR, MHI as MedianHouseHoldIncome, Jurisdic as StormwaterJurisdiction, Permit as PermitType", false);
+                NeptuneWebConfiguration.DatabaseConnectionString, "LandUseBlockStaging", "Select PLU_Cat as PriorityLandUseType, LU_Descr as LandUseDescription, TGR as TrashGenerationRate, LU_for_TGR as LandUseForTGR, MHI as MedianHouseHoldIncome, Jurisdic as StormwaterJurisdiction, Permit as PermitType", false);
 
             var landUseBlockStagings = HttpRequestStorage.DatabaseEntities.LandUseBlockStagings.ToList();
             var stormwaterJurisdictions = HttpRequestStorage.DatabaseEntities.StormwaterJurisdictions.ToList();
@@ -41,80 +41,90 @@ namespace Neptune.Web.ScheduledJobs
             var count = 0;
             var errorList = new List<string> ();
             var landUseBlocksToUpload = new List<LandUseBlock>();
-            var landUseBlockSimple = new LandUseBlock(default(int), default(int), string.Empty, default(DbGeometry), default(decimal), string.Empty, default(decimal), default(decimal), default(int), default(int));
-            foreach (var landUseBlock in landUseBlockStagings)
+            foreach (var landUseBlockStaging in landUseBlockStagings)
             {
-                var landUseBlockPLUType = landUseBlock.PriorityLandUseType;
+                var landUseBlock = new LandUseBlock(default(int), default(int), string.Empty, default(DbGeometry), default(decimal), string.Empty, default(decimal), default(decimal), default(int), default(int));
+
+
+                var landUseBlockPLUType = landUseBlockStaging.PriorityLandUseType;
                 if (!PriorityLandUseType.All.Select(x => x.PriorityLandUseTypeDisplayName).Contains(landUseBlockPLUType))
                 {
                     errorList.Add($"The Priority Land Use Type '{landUseBlockPLUType}' at row {count} was not found. Acceptable values are {string.Join(", ", PriorityLandUseType.All.Select(x => x.PriorityLandUseTypeDisplayName).ToList())}");
                 }
                 else
                 {
-                    landUseBlockSimple.PriorityLandUseTypeID = PriorityLandUseType.All
+                    landUseBlock.PriorityLandUseTypeID = PriorityLandUseType.All
                         .Single(x => x.PriorityLandUseTypeDisplayName == landUseBlockPLUType).PriorityLandUseTypeID;
                 }
 
-                landUseBlockSimple.LandUseDescription = landUseBlock.LandUseDescription;
+                landUseBlock.LandUseDescription = landUseBlockStaging.LandUseDescription;
+                
+                landUseBlock.TrashGenerationRate = landUseBlockStaging.TrashGenerationRate;
+                landUseBlock.LandUseForTGR = landUseBlockStaging.LandUseForTGR;
 
-                if (landUseBlock.LandUseBlockStagingGeometry == null)
+                if (landUseBlockStaging.LandUseForTGR == "Residential")
+                {
+                    landUseBlock.MedianHouseholdIncomeResidential = landUseBlockStaging.MedianHouseholdIncome;
+                    landUseBlock.MedianHouseholdIncomeRetail = 0;
+                }else if (landUseBlockStaging.LandUseForTGR == "Retail")
+                {
+                    landUseBlock.MedianHouseholdIncomeResidential = 0;
+                    landUseBlock.MedianHouseholdIncomeRetail = landUseBlockStaging.MedianHouseholdIncome;
+                }
+                else
+                {
+                    landUseBlock.MedianHouseholdIncomeResidential = 0;
+                    landUseBlock.MedianHouseholdIncomeRetail = 0;
+                }
+
+                var stormwaterJurisdictionName = landUseBlockStaging.StormwaterJurisdiction;
+                if (string.IsNullOrEmpty(stormwaterJurisdictionName))
+                {
+                    errorList.Add($"The Stormwater Jurisdiction at row {count} is null, empty or whitespace. A value must be provided");
+                }
+                if (!stormwaterJurisdictions.Select(x => x.Organization.OrganizationName).Contains(stormwaterJurisdictionName))
+                {
+                    errorList.Add($"The Stormwater Jurisdiction '{stormwaterJurisdictionName}' at row {count} was not found. Acceptable values are {string.Join(", ", stormwaterJurisdictions.Select(x => x.Organization.OrganizationName).ToList())}");
+                }
+                else
+                {
+                    landUseBlock.StormwaterJurisdictionID = stormwaterJurisdictions
+                        .Single(x => x.Organization.OrganizationName == landUseBlockStaging.StormwaterJurisdiction)
+                        .StormwaterJurisdictionID;
+                }
+
+                if (landUseBlockStaging.LandUseBlockStagingGeometry == null)
                 {
                     errorList.Add($"The Land Use Block Geometry at row {count} is null. A value must be provided");
                 }
                 else
                 {
-                    landUseBlockSimple.LandUseBlockGeometry = landUseBlock.LandUseBlockStagingGeometry;
-                }
-                
-                landUseBlockSimple.TrashGenerationRate = landUseBlock.TrashGenerationRate;
-                landUseBlockSimple.LandUseForTGR = landUseBlock.LandUseForTGR;
+                    var stormwaterJurisdiction = stormwaterJurisdictions
+                        .Single(x => x.StormwaterJurisdictionID == landUseBlock.StormwaterJurisdictionID);
+                    var clippedGeometry = landUseBlock.LandUseBlockGeometry = landUseBlockStaging.LandUseBlockStagingGeometry.Intersection(stormwaterJurisdiction.StormwaterJurisdictionGeometry);
 
-                if (landUseBlock.LandUseForTGR == "Residential")
-                {
-                    landUseBlockSimple.MedianHouseholdIncomeResidential = landUseBlock.MedianHouseholdIncome;
-                    landUseBlockSimple.MedianHouseholdIncomeRetail = 0;
-                }else if (landUseBlock.LandUseForTGR == "Retail")
-                {
-                    landUseBlockSimple.MedianHouseholdIncomeResidential = 0;
-                    landUseBlockSimple.MedianHouseholdIncomeRetail = landUseBlock.MedianHouseholdIncome;
-                }
-                else
-                {
-                    landUseBlockSimple.MedianHouseholdIncomeResidential = 0;
-                    landUseBlockSimple.MedianHouseholdIncomeRetail = 0;
+                    if (clippedGeometry.IsEmpty)
+                    {
+                        errorList.Add($"The Clipped Land Use Block Geometry at row {count} is null. Please make sure Land Use Block is in the '{stormwaterJurisdiction}' stormwater jurisdiction");
+                    }
+
                 }
 
-                var stormwaterJurisdiction = landUseBlock.StormwaterJurisdiction;
-                if (string.IsNullOrEmpty(stormwaterJurisdiction))
-                {
-                    errorList.Add($"The Stormwater Jurisdiction at row {count} is null, empty or whitespace. A value must be provided");
-                }
-                if (!stormwaterJurisdictions.Select(x => x.Organization.OrganizationName).Contains(stormwaterJurisdiction))
-                {
-                    errorList.Add($"The Stormwater Jurisdiction '{stormwaterJurisdiction}' at row {count} was not found. Acceptable values are {string.Join(", ", stormwaterJurisdictions.Select(x => x.Organization.OrganizationName).ToList())}");
-                }
-                else
-                {
-                    landUseBlockSimple.StormwaterJurisdictionID = stormwaterJurisdictions
-                        .Single(x => x.Organization.OrganizationName == landUseBlock.StormwaterJurisdiction)
-                        .StormwaterJurisdictionID;
-                }
-
-                var permitType = landUseBlock.PermitType;
+                var permitType = landUseBlockStaging.PermitType;
                 if (string.IsNullOrEmpty(permitType))
                 {
                     errorList.Add($"The Permit Type at row {count} is null, empty or whitespace. A value must be provided");
                 }
-                if (!PermitType.All.Select(x => x.PermitTypeDisplayName).Contains(landUseBlock.PermitType))
+                if (!PermitType.All.Select(x => x.PermitTypeDisplayName).Contains(landUseBlockStaging.PermitType))
                 {
                     errorList.Add($"The Permit Type '{permitType}' at row {count} was not found. Acceptable values are {string.Join(", ", PermitType.All.Select(x => x.PermitTypeDisplayName).ToList())}");
                 }
                 else
                 {
-                    landUseBlockSimple.PermitTypeID =
+                    landUseBlock.PermitTypeID =
                         PermitType.All.Single(x => x.PermitTypeDisplayName == permitType).PermitTypeID;
                 }
-                landUseBlocksToUpload.Add(landUseBlockSimple);
+                landUseBlocksToUpload.Add(landUseBlock);
                 count++;
             }
 
@@ -129,12 +139,11 @@ namespace Neptune.Web.ScheduledJobs
             {
                 HttpRequestStorage.DatabaseEntities.LandUseBlocks.AddRange(landUseBlocksToUpload);
                 HttpRequestStorage.DatabaseEntities.SaveChanges(person);
-                message =
-                    $"Congratulations, {count} Land Use Blocks were successfully uploaded into the Land Use Block table.";
+                message = $"Congratulations, {count} Land Use Blocks were successfully uploaded into the Land Use Block table.";
                 //sendMessage(person, message, errorList);
             }
 
-            throw new NotImplementedException();
+            HttpRequestStorage.DatabaseEntities.LandUseBlockStagings.DeleteLandUseBlockStaging(landUseBlockStagings);
         }
     }
 }
