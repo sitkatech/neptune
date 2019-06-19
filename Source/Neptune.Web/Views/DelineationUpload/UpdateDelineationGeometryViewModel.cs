673713b7c3f0131d4f98cabd1f48222b2fff27a5
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-19 13:32:19 -0700

    Refactor validation on UpdateDelineationGeometryViewModel to only check that there is a single feature class in the upload

--- Source/Neptune.Web/Views/DelineationUpload/UpdateDelineationGeometryViewModel.cs
+++ Source/Neptune.Web/Views/DelineationUpload/UpdateDelineationGeometryViewModel.cs
@@ -33,57 +33,32 @@ namespace Neptune.Web.Views.DelineationUpload
                 var gdbFile = disposableTempFile.FileInfo;
                 FileResourceData.SaveAs(gdbFile.FullName);
 
-                var ogr2OgrCommandLineRunner = new Ogr2OgrCommandLineRunner(NeptuneWebConfiguration.Ogr2OgrExecutable,
-                    Ogr2OgrCommandLineRunner.DefaultCoordinateSystemId,
-                    NeptuneWebConfiguration.HttpRuntimeExecutionTimeout.TotalMilliseconds);
-
-                List<string> featureClassNames = null;
                 try
                 {
-                    featureClassNames = OgrInfoCommandLineRunner.GetFeatureClassNamesFromFileGdb(new FileInfo(NeptuneWebConfiguration.OgrInfoExecutable),
+                    var featureClassNames = OgrInfoCommandLineRunner.GetFeatureClassNamesFromFileGdb(
+                        new FileInfo(NeptuneWebConfiguration.OgrInfoExecutable),
                         gdbFile,
                         Ogr2OgrCommandLineRunner.DefaultTimeOut);
+
+                    if (featureClassNames?.Count == 0)
+                    {
+                        errors.Add(new ValidationResult(
+                            "The file geodatabase contained no feature class. Please upload a file geodatabase containing exactly one feature class."));
+                        return errors;
+                    }
+
+                    if (featureClassNames?.Count != 1)
+                    {
+                        errors.Add(new ValidationResult(
+                            "The file geodatabase contained more than one feature class. Please upload a file geodatabase containing exactly one feature class."));
+                        return errors;
+                    }
                 }
                 catch (Exception e)
                 {
-                    errors.Add(new ValidationResult("There was a problem uploading your file geodatabase. Verify it meets the requirements and is not corrupt."));
+                    errors.Add(new ValidationResult(
+                        "There was a problem uploading your file geodatabase. Verify it meets the requirements and is not corrupt."));
                     SitkaLogger.Instance.LogDetailedErrorMessage(e);
-                }
-
-                if (featureClassNames != null)
-                {
-                    if (featureClassNames.Count == 0)
-                    {
-                        errors.Add(new ValidationResult("The file geodatabase contained no feature class. Please upload a file geodatabase containing exactly one feature class."));
-                        return errors;
-                    }
-
-                    if (featureClassNames.Count != 1)
-                    {
-                        errors.Add(new ValidationResult("The file geodatabase contained more than one feature class. Please upload a file geodatabase containing exactly one feature class."));
-                        return errors;
-                    }
-
-                    var featureClasses = featureClassNames.ToDictionary(x => x,
-                        x =>
-                        {
-                            try
-                            {
-                                var geoJson = ogr2OgrCommandLineRunner.ImportFileGdbToGeoJson(gdbFile, x, false);
-                                return JsonTools.DeserializeObject<FeatureCollection>(geoJson);
-                            }
-                            catch (Exception e)
-                            {
-                                errors.Add(new ValidationResult($"There was a problem processing the Feature Class \"{x}\"."));
-                                SitkaLogger.Instance.LogDetailedErrorMessage(e);
-                                return null;
-                            }
-                        }).Where(x => x.Value != null && DelineationGeometryStaging.IsUsableFeatureCollectionGeoJson(x.Value));
-
-                    if (!featureClasses.Any())
-                    {
-                        errors.Add(new ValidationResult("There are no usable Feature Classes in the uploaded file. Feature Classes must contain Polygon and/or Multi-Polygon features."));
-                    }
                 }
             }
 
@@ -97,7 +72,8 @@ namespace Neptune.Web.Views.DelineationUpload
                 var gdbFile = disposableTempFile.FileInfo;
                 FileResourceData.SaveAs(gdbFile.FullName);
 
-                HttpRequestStorage.DatabaseEntities.DelineationGeometryStagings.DeleteDelineationGeometryStaging(currentPerson.DelineationGeometryStagings);
+                HttpRequestStorage.DatabaseEntities.DelineationGeometryStagings.DeleteDelineationGeometryStaging(
+                    currentPerson.DelineationGeometryStagings);
                 currentPerson.DelineationGeometryStagings.Clear();
                 DelineationGeometryStaging.CreateDelineationGeometryStagingListFromGdb(gdbFile, currentPerson);
             }
