1b39e0485b8d41f7ec3066332d0a6374f3a77754
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-27 11:47:01 -0700

    Fix access rights to OVTA export page

--- Source/Neptune.Web/Areas/Trash/Controllers/OnlandVisualTrashAssessmentExportController.cs
+++ Source/Neptune.Web/Areas/Trash/Controllers/OnlandVisualTrashAssessmentExportController.cs
@@ -1,11 +1,13 @@
 ﻿using Neptune.Web.Areas.Trash.Views.OnlandVisualTrashAssessmentExport;
 using Neptune.Web.Controllers;
 using System.Web.Mvc;
+using Neptune.Web.Security;
 
 namespace Neptune.Web.Areas.Trash.Controllers
 {
     public class OnlandVisualTrashAssessmentExportController : NeptuneBaseController
     {
+        [NeptuneViewFeature]
         public ViewResult ExportAssessmentGeospatialData()
         {
 
