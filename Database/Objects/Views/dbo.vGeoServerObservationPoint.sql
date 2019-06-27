ad96c6cd9fb4dbdc15266c90c0819bcd1339b0a4
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-27 11:07:59 -0700

    [neptune/#313] Allow user to export OVTA spatial data for their jurisdiction Create GeoServer views for new downloads

---
+++ Database/Objects/Views/dbo.vGeoServerObservationPoint.sql
@@ -0,0 +1,18 @@
+
+Drop View If Exists dbo.vGeoServerObservationPoint
+go
+
+Create View dbo.vGeoServerObservationPoint
+as
+Select
+	OnlandVisualTrashAssessmentAreaName,
+	a.OnlandVisualTrashAssessmentID,
+	o.LocationPoint,
+	o.Note,
+	a.CompletedDate,
+	Score.OnlandVisualTrashAssessmentScoreDisplayName as AssessmentScore
+from
+	dbo.OnlandVisualTrashAssessmentObservation o join dbo.OnlandVisualTrashAssessment a
+		on o.OnlandVisualTrashAssessmentID = a.OnlandVisualTrashAssessmentID
+	join dbo.OnlandVisualTrashAssessmentArea area on a.OnlandVisualTrashAssessmentAreaID = area.OnlandVisualTrashAssessmentAreaID
+	join dbo.OnlandVisualTrashAssessmentScore score on a.OnlandVisualTrashAssessmentScoreID = score.OnlandVisualTrashAssessmentScoreID
---
+++ Database/Objects/Views/dbo.vGeoServerTransectLine.sql
@@ -0,0 +1,6 @@
+Drop View If Exists dbo.vGeoServerTransectLine
+Go
+
+Create View dbo.vGeoServerTransectLine as
+Select OnlandVisualTrashAssessmentAreaName, TransectLine from dbo.OnlandVisualTrashAssessmentArea
+Go
