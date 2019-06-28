69c38fa7cb6d9b7a271bd0d7d63b01fd54cf397d
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-28 11:58:06 -0700

    Add relative Photo Url to observation point view

--- Database/Objects/Views/dbo.vGeoServerObservationPointExport.sql
+++ Database/Objects/Views/dbo.vGeoServerObservationPointExport.sql
@@ -11,13 +11,21 @@ Select
 	a.CompletedDate,
 	Score.OnlandVisualTrashAssessmentScoreDisplayName as Score,
 	area.StormwaterJurisdictionID as JurisID,
-	org.OrganizationName as JurisName
+	org.OrganizationName as JurisName,
+	case
+		when fr.FileResourceGUID is not null then concat( '/FileResource/DisplayResource/', fr.FileResourceGUID) 
+		else null
+	end as PhotoUrl
 from
 	dbo.OnlandVisualTrashAssessmentObservation o join dbo.OnlandVisualTrashAssessment a
 		on o.OnlandVisualTrashAssessmentID = a.OnlandVisualTrashAssessmentID
 	join dbo.OnlandVisualTrashAssessmentArea area on a.OnlandVisualTrashAssessmentAreaID = area.OnlandVisualTrashAssessmentAreaID
 	join dbo.OnlandVisualTrashAssessmentScore score on a.OnlandVisualTrashAssessmentScoreID = score.OnlandVisualTrashAssessmentScoreID
 	left join dbo.StormwaterJurisdiction sj
-			on area.StormwaterJurisdictionID = sj.StormwaterJurisdictionID
-		left join dbo.Organization org
-			on sj.OrganizationID = org.OrganizationID
+		on area.StormwaterJurisdictionID = sj.StormwaterJurisdictionID
+	left join dbo.Organization org
+		on sj.OrganizationID = org.OrganizationID
+	left join dbo.OnlandVisualTrashAssessmentObservationPhoto photo
+		on photo.OnlandVisualTrashAssessmentObservationID = o.OnlandVisualTrashAssessmentObservationID
+	left join dbo.FileResource fr
+		on fr.FileResourceID = photo.FileResourceID
