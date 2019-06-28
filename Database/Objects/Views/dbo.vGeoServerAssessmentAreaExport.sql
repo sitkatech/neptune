9264fb991bf82395aae3cdfd2a5521b575494f0d
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-06-28 10:15:50 -0700

    Create new vGeoSeverAssessmentAreaExport view; rename files of other export views

---
+++ Database/Objects/Views/dbo.vGeoServerAssessmentAreaExport.sql
@@ -0,0 +1,27 @@
+DROP VIEW IF EXISTS dbo.vGeoServerAssessmentAreaExport
+GO
+
+create view dbo.vGeoServerAssessmentAreaExport as
+	Select
+		area.OnlandVisualTrashAssessmentAreaID,
+		area.OnlandVisualTrashAssessmentAreaName,
+		area.StormwaterJurisdictionID,
+		area.OnlandVisualTrashAssessmentAreaGeometry,
+		score.OnlandVisualTrashAssessmentScoreDisplayName as Score,
+		ovta.OnlandVisualTrashAssessmentID,
+		ovta.CompletedDate,
+		ovta.IsProgressAssessment
+	from dbo.OnlandVisualTrashAssessmentArea area
+		left join (
+			Select
+				*,
+				Row_Number() over (partition by OnlandVisualTrashAssessmentAreaID order by CompletedDate desc) as RankByCompletedDate
+			from dbo.OnlandVisualTrashAssessment
+			where CompletedDate is not null
+		) ovta on area.OnlandVisualTrashAssessmentAreaID = ovta.OnlandVisualTrashAssessmentAreaID
+		left join dbo.OnlandVisualTrashAssessmentScore score
+			on ovta.OnlandVisualTrashAssessmentScoreID = score.OnlandVisualTrashAssessmentScoreID
+	where 
+		RankByCompletedDate between 1 and 5
+		or RankByCompletedDate is null -- have to account for this being null so we get the results of the left outer join
+go
