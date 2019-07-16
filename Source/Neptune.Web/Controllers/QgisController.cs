741176a1f6fafb58f061b1462b10a0a7dcea823e
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-07-16 12:47:35 -0700

    [neptune/#424] Install QGIS and run a simple command from web app Create a sitka-admin-only endpoint to faciliate manual testing

---
+++ Source/Neptune.Web/Controllers/QgisController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Neptune.Web.Common;
+using Neptune.Web.Security;
+
+namespace Neptune.Web.Controllers
+{
+    public class QgisController : NeptuneBaseController
+    {
+        [HttpGet]
+        [SitkaAdminFeature]
+        public ContentResult Test()
+        {
+            var processUtilityResult = QgisRunner.ExecutePyqgisScript(NeptuneWebConfiguration.PathToPyqgisTestScript, NeptuneWebConfiguration.PyqgisTestWorkingDirectory);
+
+            if (processUtilityResult.ReturnCode == 0 && processUtilityResult.StdOut.Contains("Success") &&
+                processUtilityResult.StdOut.Contains("CatchIDN"))
+            {
+                return Content("Pyqgis execution succeeded.");
+            }
+            else
+            {
+                return Content("Pyqgis execution failed.");
+            }
+        }
+    }
+}
