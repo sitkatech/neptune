5ba103aa885a0e54d765b0971107d4f6a9b73ae2
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-07-22 10:43:53 -0700

    [neptune/#415] Geoprocessing improvements to the TGU pipeline Add defaults to vDelineationTGUInput's TGR

---
+++ Database/Objects/Views/dbo.vDelineationTGUInput.sql
@@ -0,0 +1,24 @@
+Drop View If Exists dbo.vDelineationTGUInput
+Go
+
+Create View [dbo].[vDelineationTGUInput]
+As
+Select
+	d.DelineationID,
+	DelineationGeometry,
+	t.StormwaterJurisdictionID,
+	ISNULL(Case
+		when tcs.TrashCaptureStatusTypeDisplayName = 'Full' then 100
+		when tcs.TrashCaptureStatusTypeDisplayName = 'None' or tcs.TrashCaptureStatusTypeDisplayName = 'Not Provided' then 0
+		when t.TrashCaptureEffectiveness is Null then 0
+		else t.TrashCaptureEffectiveness
+	end, 0.0) as TrashCaptureEffectiveness,
+	tcs.TrashCaptureStatusTypeDisplayName
+From dbo.Delineation d
+	join dbo.TreatmentBMP t
+		on d.TreatmentBMPID = t.TreatmentBMPID
+	join dbo.TrashCaptureStatusType tcs
+		on tcs.TrashCaptureStatusTypeID = t.TrashCaptureStatusTypeID
+	Where IsVerified = 1
+GO
+
