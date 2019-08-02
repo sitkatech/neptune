d2839e95a82b445743b115f97605c997c5e54bf5
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-08-02 11:38:23 -0700

    [neptune/#430] Move metrics to Neighborhood panel and implement 3 slide out panels Refactor original slideout treatment as a jQuery plugin

---
+++ Source/Neptune.Web/Content/JQuery/jquery.slideout/jquery.slideout.js
@@ -0,0 +1,28 @@
+﻿(function ($) {
+
+    $.fn.slideout = function (options) {
+        var targetSelector = options.targetSelector;
+
+        $(targetSelector).append("<div class='slideout-wrap'></div>");
+        $(targetSelector + " .slideout-wrap").append(this);
+        this.addClass('slideout-content');
+        $(targetSelector).append("<span class='expando-bar'> --&gt; </span>");
+
+        $(targetSelector + " .expando-bar").on("click",
+            function() {
+                this.expand = !this.expand;
+
+                if (this.expand) {
+                    $(targetSelector + " .slideout-wrap").addClass("slideout-expant");
+                    $(this).removeClass("glyphicon-menu-right");
+                    $(this).addClass("glyphicon-menu-left");
+
+                } else {
+                    $(targetSelector + " .slideout-wrap").removeClass("slideout-expant");
+                    $(this).addClass("glyphicon-menu-right");
+                    $(this).removeClass("glyphicon-menu-left");
+                }
+            });
+    };
+
+}(jQuery));
