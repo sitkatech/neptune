d40cc46ee83991759f9ed60dcee8552a4bcd410f
raynardlee <raynardlee@yahoo.com>
2019-10-01 10:53:18 -0700

    adding qa support and qa tester groups to restore script

--- Database/Baseline/config_roles.sql
+++ Database/Baseline/config_roles.sql
@@ -6,6 +6,8 @@ select '${db-user}' as AccountName
 into #accountsToCreate
 union select '${db-batch-user}'
 union select '${db-geoserver-user}'
+union select 'Sitka\H20 QA Support'
+union select 'Sitka\H20 QA Tester'
 
 -- Clear out any existing logins
 print 'Clearing out any existing logins'
