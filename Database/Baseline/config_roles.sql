58645e984632ab2ebbf1599a4d0d1c4364f60185
raynardlee <raynardlee@yahoo.com>
2019-10-01 11:56:25 -0700

    fix to the config_roles script to drop the qa tester groups

--- Database/Baseline/config_roles.sql
+++ Database/Baseline/config_roles.sql
@@ -26,6 +26,8 @@ from
 	union select '${local-db-geoserver-user}'
 	union select '${qa-db-geoserver-user}'
 	union select '${prod-db-geoserver-user}'
+    union select 'Sitka\H20 QA Support'
+    union select 'Sitka\H20 QA Tester'
 ) a left join #accountsToCreate ac on a.AccountName = ac.AccountName
 where ac.AccountName is null
 
