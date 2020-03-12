79f338beb1c90b8f2dad05c3fb103c26f69ac0fb
raynardlee <raynardlee@yahoo.com>
2020-03-12 13:46:35 -0700

    [neptune/#534] Spike Power BI: Create a simple API that can be consumed analyzed giving Kathleen a webservice token

--- Database/ReleaseScript/0240 - add webserviceaccesstoken.sql
+++ Database/ReleaseScript/0240 - add webserviceaccesstoken.sql
@@ -3,3 +3,6 @@ GO
 
 update dbo.Person set WebServiceAccessToken = '709859C8-7376-4709-A298-7606E820DA05'
 where PersonID = 1
+
+update dbo.Person set WebServiceAccessToken = '668001A5-3C11-4DCA-91BD-F8C07C11A7F3'
+where PersonID = 1095
