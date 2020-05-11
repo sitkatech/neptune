184cef0bd577ce202bdc07146010e39208b4a839
raynardlee <raynardlee@yahoo.com>
2020-05-11 16:45:25 -0700

    removed unused code

--- Source/Neptune.Web/Common/TreatmentBMPCsvParserHelper.cs
+++ Source/Neptune.Web/Common/TreatmentBMPCsvParserHelper.cs
@@ -784,22 +784,6 @@ namespace Neptune.Web.Common
             }
             return returnList;
         }
-
-        private static readonly Dictionary<string, int> monthsToInt = new Dictionary<string, int>()
-        {
-            { "Jan", 1 },
-            { "Feb", 2 },
-            { "Mar", 3 },
-            { "Apr", 4 },
-            { "May", 5 },
-            { "Jun", 6 },
-            { "Jul", 7 },
-            { "Aug", 8 },
-            { "Sep", 9 },
-            { "Oct", 10 },
-            { "Nov", 11 },
-            { "Dec", 12 }
-        };
     }
 
 }
