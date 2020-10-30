390c924fd4cfe23da6cb1bacc5cfa691a19475a7
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-10-30 12:53:23 -0700

    Add geometry_columns entries for OVTA export views

---
+++ Database/ReleaseScript/0264 - Add geometry_columns entries for OVTA export views.sql
@@ -0,0 +1,6 @@
+
+Insert into geometry_columns (f_table_catalog, f_table_schema, f_table_name, f_geometry_column, coord_dimension, srid, geometry_type)
+values
+('Neptune', 'dbo', 'vGeoServerAssessmentAreaExport', 'OnlandVisualTrashAssessmentAreaGeometry', 2, 2771, 'MULTIPOLYGON'),
+('Neptune', 'dbo', 'vGeoServerObservationPointExport', 'LocationPoint', 2, 2771, 'POINT'),
+('Neptune', 'dbo', 'vGeoServerTransectLineExport', 'TransectLine', 2, 2771, 'LINESTRING')
