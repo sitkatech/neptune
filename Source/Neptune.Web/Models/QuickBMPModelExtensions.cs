b1d9d498e3be64b46fb01a2158b1fb86d3517381
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-11-20 15:02:46 -0800

    Fix Linq-to-Entities issue

--- Source/Neptune.Web/Models/QuickBMPModelExtensions.cs
+++ Source/Neptune.Web/Models/QuickBMPModelExtensions.cs
@@ -1,10 +1,19 @@
-﻿namespace Neptune.Web.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Neptune.Web.Models
 {
     public static class QuickBMPModelExtensions
     {
-        public static bool IsFullyParameterized(this QuickBMP x)
+        public static bool IsFullyParameterized(this QuickBMP x) =>
+            x.PercentOfSiteTreated != null && x.TreatmentBMPType.IsAnalyzedInModelingModule;
+
+        // It's irritating that this code can't share the above code
+        // but an answer to this SE says it should be can: https://stackoverflow.com/questions/31399955/linq-to-entities-does-not-support-extension-method
+        public static List<QuickBMP> GetFullyParameterized(this IQueryable<QuickBMP> quickBMPs)
         {
-            return x.PercentOfSiteTreated != null && x.TreatmentBMPType.IsAnalyzedInModelingModule;
+            return quickBMPs.Where(x => x.PercentOfSiteTreated != null && x.TreatmentBMPType.IsAnalyzedInModelingModule)
+                .ToList();
         }
     }
 }
