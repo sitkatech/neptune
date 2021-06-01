f6ea4266e3e1bccfcf19d2e036e4e64924715dcf
Mack Peters <mack.peters@sitkatech.com>
2021-06-01 09:17:17 -0700

    Was joining a the wqmpverify table on itself when we should've been joining the wqmpid. Addressed and the results are much smaller

--- Database/Objects/Views/dbo.vPowerBIWaterQualityManagementPlanOAndMVerification.sql
+++ Database/Objects/Views/dbo.vPowerBIWaterQualityManagementPlanOAndMVerification.sql
@@ -21,7 +21,7 @@ from dbo.WaterQualityManagementPlanVerify wqmpv
 join dbo.WaterQualityManagementPlanVerifyType wqmpvt on wqmpv.WaterQualityManagementPlanVerifyTypeID = wqmpvt.WaterQualityManagementPlanVerifyTypeID
 left join dbo.WaterQualityManagementPlanVerifyStatus wqmpvs on wqmpv.WaterQualityManagementPlanVerifyStatusID = wqmpvs.WaterQualityManagementPlanVerifyStatusID
 join dbo.WaterQualityManagementPlanVisitStatus wqmpvss on wqmpv.WaterQualityManagementPlanVisitStatusID = wqmpvss.WaterQualityManagementPlanVisitStatusID
-join dbo.WaterQualityManagementPlan wqmp on wqmpv.WaterQualityManagementPlanID = wqmpv.WaterQualityManagementPlanID
+join dbo.WaterQualityManagementPlan wqmp on wqmpv.WaterQualityManagementPlanID = wqmp.WaterQualityManagementPlanID
 left join dbo.Person p on wqmpv.LastEditedByPersonID = p.PersonID
 join dbo.StormwaterJurisdiction sj on wqmp.StormwaterJurisdictionID = sj.StormwaterJurisdictionID
 join dbo.Organization o on sj.OrganizationID = o.OrganizationID
