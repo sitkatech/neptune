b2bc6f2caee5feef3d28668dc91efb3be563fd31
Mack Peters <mack.peters@sitkatech.com>
2021-11-29 11:56:54 -0800

    Add a verify view feature that  looks like the old wqmp  view feature, make  sure we don't delete entities ahead of a larger delete which causes entity framework to get upset because it's trying to delete something  that doesn't exist

---
+++ Source/Neptune.Web/Security/WaterQualityManagementPlanVerifyViewFeature.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Neptune.Web.Models;
+
+namespace Neptune.Web.Security
+{
+    [SecurityFeatureDescription("Allows viewing a WQMP  Verification if you are a user with a role")]
+    public class WaterQualityManagementPlanVerifyViewFeature : NeptuneFeature
+    {
+        public WaterQualityManagementPlanVerifyViewFeature()
+            : base(new HashSet<Role> { Role.JurisdictionEditor, Role.JurisdictionManager, Role.Admin, Role.SitkaAdmin })
+        {
+        }
+    }
+}
