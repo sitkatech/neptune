9237a31892df0cddda44243e9a626beeefd5f7e8
John Burns <john.burns@sitkatech.com>
2022-01-12 13:16:58 -0800

    Exclude LSPC Basins from the "overlay rodeo" in ModelingOverlayAnalysis; instead rely on the foreign-key relationship between RSBs and LSPCs that is already calculated in pUpdateRegionalSubbasinIntersectionCache when RSBs are updated.

--- Database/Objects/Views/dbo.vRegionalSubbasinLGUInput.sql
+++ Database/Objects/Views/dbo.vRegionalSubbasinLGUInput.sql
@@ -5,6 +5,7 @@ Create view dbo.vRegionalSubbasinLGUInput
 as
 Select
 	RegionalSubbasinID as RSBID,
-	CatchmentGeometry
+	CatchmentGeometry,
+	LSPCBasinID as LSPCID -- Must rename fields to have less than 10 characters for compatiblity with shapefile format
 From dbo.RegionalSubbasin
 GO
