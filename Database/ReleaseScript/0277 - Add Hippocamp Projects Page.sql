4fa7a82324e0b3363fccff0a14bfbef4d3488383
jamie.quishenberry <jamie.quishenberry@sitkatech.com>
2022-01-25 13:59:20 -0800

    Added Hippocamp Project List page to NeptunePage/NeptunePageType tables and corresponding lookup table.

---
+++ Database/ReleaseScript/0277 - Add Hippocamp Projects Page.sql
@@ -0,0 +1,5 @@
+insert into dbo.NeptunePageType(NeptunePageTypeID, NeptunePageTypeName, NeptunePageTypeDisplayName)
+values (56, 'HippocampProjectsList', 'Hippocamp Projects List')
+
+insert into dbo.NeptunePage(NeptunePageTypeID, NeptunePageContent)
+values (56, '<p>A list of all projects in your assigned jurisdictions.</p>')
