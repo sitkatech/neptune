00e9700d4c4111861b333d03673295121218bf01
Mack Peters <mack.peters@sitkatech.com>
2022-02-08 14:36:35 -0800

    Add change that was reverted but shouldn't have been

--- Hippocamp.API/Services/Authorization/UserViewFeature.cs
+++ Hippocamp.API/Services/Authorization/UserViewFeature.cs
@@ -4,7 +4,7 @@ namespace Hippocamp.API.Services.Authorization
 {
     public class UserViewFeature : BaseAuthorizationAttribute
     {
-        public UserViewFeature() : base(new []{RoleEnum.Admin, RoleEnum.SitkaAdmin, RoleEnum.Unassigned})
+        public UserViewFeature() : base(new []{RoleEnum.Admin, RoleEnum.SitkaAdmin, RoleEnum.JurisdictionManager, RoleEnum.JurisdictionEditor, RoleEnum.Unassigned})
         {
         }
     }
