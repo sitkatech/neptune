8661abd0b521be32a3761e0d355cb92d96002069
Mack Peters <mack.peters@sitkatech.com>
2022-04-13 16:11:36 -0700

    Update our geoserver layer to actually have everything we want in it

--- Database/Objects/Views/dbo.vGeoServerOCTAPrioritization.sql
+++ Database/Objects/Views/dbo.vGeoServerOCTAPrioritization.sql
@@ -6,9 +6,16 @@ Select
 	o.OCTAPrioritizationID,
 	o.OCTAPrioritizationKey,
 	o.OCTAPrioritizationGeometry4326 as OCTAPrioritizationGeometry,
+	o.CatchIDN,
+	o.Watershed,
 	o.TPI as TransportationNexusScore,
 	o.WQNLU as LandUseBasedWaterQualityNeedScore,
 	o.WQNMON as ReceivingWaterScore,
-	o.SEA as StrategicallyEffectiveAreaScore
+	o.SEA as StrategicallyEffectiveAreaScore,
+	o.PC_BAC_PCT,
+	o.PC_MET_PCT,
+	o.PC_NUT_PCT,
+	o.PC_TSS_PCT,
+	o.PC_VOL_PCT
 From dbo.OCTAPrioritization o
 GO
