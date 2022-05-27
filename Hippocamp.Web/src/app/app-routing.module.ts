afa70b7f02f25d0ac65a4e01e846874fe5abdfa1
jamie.quishenberry <jamie.quishenberry@sitkatech.com>
2022-05-27 08:41:47 -0700

    OCTA M2 Tier 2 Grant Program dashboard component created & laid out with copy of planning map

--- Hippocamp.Web/src/app/app-routing.module.ts
+++ Hippocamp.Web/src/app/app-routing.module.ts
@@ -25,6 +25,7 @@ import { UnsavedChangesGuard } from './shared/guards/unsaved-changes.guard';
 import { ProjectDetailComponent } from './pages/projects/project-detail/project-detail.component';
 import { ReviewComponent } from './pages/projects/project-workflow/review/review.component';
 import { PlanningMapComponent } from './pages/planning-map/planning-map.component';
+import { OCTAM2Tier2DashboardComponent } from './pages/grant-programs/octa-m2-tier2-dashboard/octa-m2-tier2-dashboard.component';
 
 export const routeParams = {
   definitionID: ':definitionID',
@@ -63,6 +64,12 @@ const routes: Routes = [
         ]
       },
       { path: `${routeParams.projectID}`, component: ProjectDetailComponent},
+    ]
+  },
+  {
+    path: "grant-programs", canActivate: [UnauthenticatedAccessGuard],
+    children: [
+      { path: "octa-m2-tier-2", component: OCTAM2Tier2DashboardComponent, canActivate: [JurisdictionManagerOrEditorOnlyGuard] },
     ]
   },
   { path: "planning-map", component: PlanningMapComponent, canActivate: [UnauthenticatedAccessGuard, JurisdictionManagerOrEditorOnlyGuard] },
