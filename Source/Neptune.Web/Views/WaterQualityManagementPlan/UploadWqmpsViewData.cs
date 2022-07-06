d6e471293d372dd49cbb4546253cf6774c1dcef7
Andy Schultheiss <andy.schultheiss@sitkatech.com>
2022-07-06 14:43:32 -0700

    fixed formatting for bulk upload rich text, added cancel button to bulk upload page to return to index

--- Source/Neptune.Web/Views/WaterQualityManagementPlan/UploadWqmpsViewData.cs
+++ Source/Neptune.Web/Views/WaterQualityManagementPlan/UploadWqmpsViewData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using Neptune.Web.Common;
+using Neptune.Web.Controllers;
 using Neptune.Web.Models;
 
 namespace Neptune.Web.Views.WaterQualityManagementPlan
@@ -7,15 +9,16 @@ namespace Neptune.Web.Views.WaterQualityManagementPlan
     public class UploadWqmpsViewData : NeptuneViewData
     {
         public string WqmpsUploadUrl { get; }
+        public string WqmpIndexUrl { get; }
         public List<string> ErrorList { get; }
-
-
-
+        
         public UploadWqmpsViewData(Person currentPerson, List<string> errorList, Models.NeptunePage neptunePage, string wqmpsUploadUrl) : base(currentPerson, neptunePage, NeptuneArea.OCStormwaterTools)
         {
             PageTitle = "WQMP Bulk Upload";
             EntityName = $"{FieldDefinitionType.WaterQualityManagementPlan.GetFieldDefinitionLabelPluralized()}";
             WqmpsUploadUrl = wqmpsUploadUrl;
+            WqmpIndexUrl = SitkaRoute<WaterQualityManagementPlanController>.BuildUrlFromExpression(c =>
+                c.Index());
             ErrorList = errorList;
         }
     }
