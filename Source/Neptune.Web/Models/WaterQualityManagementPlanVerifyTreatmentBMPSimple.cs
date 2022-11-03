069e2c41efb32dd85b2d565374f21abda3c29ab2
raynardlee <raynardlee@yahoo.com>
2022-11-03 11:44:40 -0700

    NPT 783 cherry pick:  Even grabbing the correct fieldVisit, it's not safe to assume that a treatment BMP with a field visit has an associated TreatmentBMPAssessment record.

--- Source/Neptune.Web/Models/WaterQualityManagementPlanVerifyTreatmentBMPSimple.cs
+++ Source/Neptune.Web/Models/WaterQualityManagementPlanVerifyTreatmentBMPSimple.cs
@@ -32,7 +32,7 @@ namespace Neptune.Web.Models
 
             var mostRecentFieldVisit = treatmentBMP.FieldVisits.Where(x => x.FieldVisitStatus == FieldVisitStatus.Complete).OrderByDescending(x => x.VisitDate).FirstOrDefault();
             FieldVisiLastVisitedtDate = mostRecentFieldVisit?.VisitDate.ToShortDateString();
-            FieldVisitMostRecentScore = mostRecentFieldVisit?.GetPostMaintenanceAssessment() != null ? mostRecentFieldVisit.GetPostMaintenanceAssessment().FormattedScore() :  mostRecentFieldVisit?.GetInitialAssessment().FormattedScore();
+            FieldVisitMostRecentScore = mostRecentFieldVisit?.GetPostMaintenanceAssessment() != null ? mostRecentFieldVisit.GetPostMaintenanceAssessment().FormattedScore() : mostRecentFieldVisit?.GetInitialAssessment()?.FormattedScore();
             TreatmentBMPDetailUrl = SitkaRoute<TreatmentBMPController>.BuildUrlFromExpression(c =>
                 c.Detail(treatmentBMP.PrimaryKey));
         }
