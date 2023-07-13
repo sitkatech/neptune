e8d2cc5c9101d32bb003ffa6e65ac19c96eccbd1
liz-arikawa-esa <larikawa@esassoc.com>
2023-07-13 14:54:51 -0700

    Generator drift

---
+++ Hippocamp.EFModels/Entities/Generated/vPyQgisOnlandVisualTrashAssessmentAreaDated.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+
+namespace Hippocamp.EFModels.Entities
+{
+    [Keyless]
+    public partial class vPyQgisOnlandVisualTrashAssessmentAreaDated
+    {
+        public int OVTAID { get; set; }
+        [Required]
+        [Column(TypeName = "geometry")]
+        public Geometry OnlandVisualTrashAssessmentAreaGeometry { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? AssessDate { get; set; }
+    }
+}
---
+++ Hippocamp.EFModels/Entities/Generated/vPyQgisWaterQualityManagementPlanTGUInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+
+namespace Hippocamp.EFModels.Entities
+{
+    [Keyless]
+    public partial class vPyQgisWaterQualityManagementPlanTGUInput
+    {
+        public int WQMPID { get; set; }
+        [Column(TypeName = "geometry")]
+        public Geometry WaterQualityManagementPlanBoundary { get; set; }
+        public int TCEffect { get; set; }
+    }
+}
