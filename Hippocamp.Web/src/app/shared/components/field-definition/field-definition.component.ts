332f7b5ced3c6a56c952c9ebbdd984b84b29913a
liz-arikawa-esa <larikawa@esassoc.com>
2023-08-01 14:06:33 -0700

    Fix field definitions so that users have to click on them to see them If a user clicks on a field definition while having a different one opened then it will close the previous one

--- Hippocamp.Web/src/app/shared/components/field-definition/field-definition.component.ts
+++ Hippocamp.Web/src/app/shared/components/field-definition/field-definition.component.ts
@@ -1,117 +1,108 @@
-import { Component, OnInit, Input, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
-import { Alert } from '../../models/alert';
-import { FieldDefinitionService } from '../../services/field-definition-service';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { AlertService } from '../../services/alert.service';
-import { AlertContext } from '../../models/enums/alert-context.enum';
-import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
-import { FieldDefinitionTypeEnum } from '../../models/enums/field-definition-type.enum';
-import { FieldDefinitionDto, PersonDto } from '../../generated/model/models';
+import { Component, OnInit, Input, ViewChild, ChangeDetectorRef } from "@angular/core";
+import { EditorComponent } from "@tinymce/tinymce-angular";
+import { AuthenticationService } from "src/app/services/authentication.service";
+import TinyMCEHelpers from "../../helpers/tiny-mce-helpers";
+import { Alert } from "../../models/alert";
+import { AlertContext } from "../../models/enums/alert-context.enum";
+import { AlertService } from "../../services/alert.service";
+import { FieldDefinitionDto } from "../../generated/model/field-definition-dto";
+import { PersonDto } from "../../generated/model/person-dto";
+import { FieldDefinitionService } from "../../services/field-definition-service";
+import { FieldDefinitionTypeEnum } from "../../models/enums/field-definition-type.enum";
+import { NgbPopover } from "@ng-bootstrap/ng-bootstrap/popover/popover";
 
-
-declare var $ : any
+declare var $: any;
 
 @Component({
-  selector: 'field-definition',
-  templateUrl: './field-definition.component.html',
-  styleUrls: ['./field-definition.component.scss']
+    selector: "field-definition",
+    templateUrl: "./field-definition.component.html",
+    styleUrls: ["./field-definition.component.scss"],
 })
 export class FieldDefinitionComponent implements OnInit {
+    @Input() fieldDefinitionType: string;
+    @Input() labelOverride: string;
+    @Input() inline: boolean = true;
+    @Input() white?: boolean;
+    @ViewChild('tinyMceEditor') tinyMceEditor : EditorComponent;
+    @ViewChild('p') public popover: NgbPopover;
+    @ViewChild('popContent') public content: any;
+    public tinyMceConfig: object;
 
-  @Input() fieldDefinitionType: string;
-  @Input() labelOverride: string;
-  @Input() labelAppend: string;
-  @Input() white?: boolean;
-  @ViewChild('p') public popover: NgbPopover;
-  @ViewChild('popContent') public content: any;
-  public fieldDefinition: FieldDefinitionDto;
-  public isLoading: boolean = true;
-  public isEditing: boolean = false;
-  public emptyContent: boolean = false;
-  public watchUserChangeSubscription: any;
-  public editedContent: string;
-  public editor;
+    private currentUser: PersonDto;
 
-  currentUser: PersonDto;
+    public fieldDefinition: FieldDefinitionDto;
+    public isLoading: boolean = true;
+    public isEditing: boolean = false;
+    public emptyContent: boolean = false;
 
-  constructor(
-    private fieldDefinitionService: FieldDefinitionService,
-    private authenticationService: AuthenticationService,
-    private cdr: ChangeDetectorRef,
-    private alertService: AlertService,
-    private elem: ElementRef
-  ) { }
+    public editedContent: string;
 
-  ngOnInit() {
-    this.fieldDefinitionService.getFieldDefinition(FieldDefinitionTypeEnum[this.fieldDefinitionType]).subscribe(x => {
-      this.loadFieldDefinition(x);
-    });
-  }
+    constructor(
+        private fieldDefinitionService: FieldDefinitionService,
+        private authenticationService: AuthenticationService,
+        private cdr: ChangeDetectorRef,
+        private alertService: AlertService
+    ) {}
 
-  ngOnDestroy() {
-    
-    this.cdr.detach();
-  }
+    ngAfterViewInit(): void {
+        // We need to use ngAfterViewInit because the image upload needs a reference to the component
+        // to setup the blobCache for image base64 encoding
+        this.tinyMceConfig = TinyMCEHelpers.DefaultInitConfig(this.tinyMceEditor)
+      }
 
-  public getLabelText() {
-    return (this.labelOverride !== null && this.labelOverride !== undefined ? this.labelOverride : this.fieldDefinition.FieldDefinitionType.FieldDefinitionTypeDisplayName) + (this.labelAppend != null && this.labelAppend != undefined ? this.labelAppend : "");
-  }
+    ngOnInit() {
+        this.authenticationService.getCurrentUser().subscribe((currentUser) => {
+            this.currentUser = currentUser;
+        });
 
-  public showEditButton(): boolean {
-    return this.authenticationService.isCurrentUserAnAdministrator();
-  }
-
-  public enterEdit(): void {
-    this.editedContent = this.fieldDefinition.FieldDefinitionValue ?? "";
-    this.isEditing = true;
-  }
-
-  public cancelEdit(): void {
-    this.isEditing = false;
-    this.popover.close();
-  }
-
-  public saveEdit(): void {
-    this.isEditing = false;
-    this.isLoading = true;
-    this.fieldDefinition.FieldDefinitionValue = this.editedContent;
-    this.fieldDefinitionService.updateFieldDefinition(this.fieldDefinition).subscribe(x => {
-      this.loadFieldDefinition(x);
-    }, error => {
-      this.isLoading = false;
-      this.alertService.pushAlert(new Alert("There was an error updating the field definition", AlertContext.Danger, true));
-    });
-  }
-
-  private loadFieldDefinition(fieldDefinition:FieldDefinitionDto)
-  {
-    this.fieldDefinition = fieldDefinition;
-    this.emptyContent = fieldDefinition.FieldDefinitionValue?.length > 0 ? false : true;
-    this.isLoading = false;
-  }
-
-  public notEditingMouseEnter() {
-    if (!this.isEditing) {
-      this.popover.open();
-      this.elem.nativeElement.closest('body')
-                             .querySelector(".popover")
-                             .addEventListener('mouseleave', this.mouseLeaveEvent.bind(this));
+        this.fieldDefinitionService.getFieldDefinition(
+            FieldDefinitionTypeEnum[this.fieldDefinitionType]
+        ).subscribe(x => this.loadFieldDefinition(x));
     }
-  }
 
-  public mouseLeaveEvent() {
-    if (!this.isEditing) {
-      this.popover.close();
+    ngOnDestroy() {
+        this.cdr.detach();
     }
-  }
 
-  public notEditingMouseLeave() {
-      setTimeout( () => {
-        let hoveringPopover = this.elem.nativeElement.closest('body')
-                                                     .querySelector(".popover:hover")
-        if (!hoveringPopover && !this.isEditing) {
-            this.popover.close();
+    private loadFieldDefinition(fieldDefinition: FieldDefinitionDto) {
+        this.fieldDefinition = fieldDefinition;
+        this.emptyContent = fieldDefinition.FieldDefinitionValue?.length == 0;
+        this.isLoading = false;
+    }
+
+    public getLabelText() {
+        if (this.labelOverride !== null && this.labelOverride !== undefined) {
+            return this.labelOverride;
         }
-    }, 50);
-  }
+
+        return this.fieldDefinition.FieldDefinitionType.FieldDefinitionTypeDisplayName;
+    }
+
+    public showEditButton(): boolean {
+        return this.authenticationService.isCurrentUserAnAdministrator();
+    }
+
+    public enterEdit(event: any): void {
+        event.preventDefault();
+
+        this.editedContent = this.fieldDefinition.FieldDefinitionValue;
+        this.isEditing = true;
+    }
+
+    public cancelEdit(): void {
+        this.isEditing = false;
+    }
+
+    public saveEdit(): void {
+        this.isEditing = false;
+        this.isLoading = true;
+        
+        this.fieldDefinition.FieldDefinitionValue = this.editedContent;
+        this.fieldDefinitionService.updateFieldDefinition(this.fieldDefinition).subscribe(x => this.loadFieldDefinition(x),
+            error => {
+                this.isLoading = false;
+                this.alertService.pushAlert(new Alert("There was an error updating the field definition", AlertContext.Danger));
+            }
+        );
+    }
 }
