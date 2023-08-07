8f24317d6ea4881cce345dc07306d03e27a209ff
raynardlee <raynardlee@yahoo.com>
2023-08-07 11:30:08 -0700

    need to add AuditDescription for WQMPBoundary to allow for deletes

---
+++ Source/Neptune.Web/Models/WaterQualityManagementPlanBoundary.cs
@@ -0,0 +1,9 @@
+﻿namespace Neptune.Web.Models;
+
+public partial class WaterQualityManagementPlanBoundary : IAuditableEntity
+{
+    public string GetAuditDescriptionString()
+    {
+        return $"Water Quality Management Plan \"{WaterQualityManagementPlanID}\" geometry deleted";
+    }
+}
