40257bd255634fa4f3857e75bf602b576ba6a410
raynardlee <raynardlee@yahoo.com>
2023-09-21 10:27:36 -0700

    removing unused view

--- Neptune.EFModels/Entities/Generated/NeptuneDbContext.cs
+++ Neptune.EFModels/Entities/Generated/NeptuneDbContext.cs
@@ -251,8 +251,6 @@ public partial class NeptuneDbContext : DbContext
 
     public virtual DbSet<vTreatmentBMPUpstream> vTreatmentBMPUpstreams { get; set; }
 
-    public virtual DbSet<vViewTreatmentBMPModelingAttribute> vViewTreatmentBMPModelingAttributes { get; set; }
-
     public virtual DbSet<vWaterQualityManagementPlanDetailed> vWaterQualityManagementPlanDetaileds { get; set; }
 
     public virtual DbSet<vWaterQualityManagementPlanLGUAudit> vWaterQualityManagementPlanLGUAudits { get; set; }
@@ -1163,11 +1161,6 @@ public partial class NeptuneDbContext : DbContext
         modelBuilder.Entity<vTreatmentBMPUpstream>(entity =>
         {
             entity.ToView("vTreatmentBMPUpstream");
-        });
-
-        modelBuilder.Entity<vViewTreatmentBMPModelingAttribute>(entity =>
-        {
-            entity.ToView("vViewTreatmentBMPModelingAttributes");
         });
 
         modelBuilder.Entity<vWaterQualityManagementPlanDetailed>(entity =>
