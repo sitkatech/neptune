c2fba3858c3f5951e32446ce8acacaa01f212966
liz-arikawa-esa <larikawa@esassoc.com>
2023-10-02 13:22:14 -0700

    add back field definitions for tests that were using them

---
+++ Neptune.EFModels/Entities/FieldDefinition.cs
@@ -0,0 +1,14 @@
+﻿namespace Neptune.EFModels.Entities;
+
+public partial class FieldDefinition
+{
+    public FieldDefinition()
+    {
+
+    }
+    public FieldDefinition(int fieldDefinitionID, string fieldDefinitionDataValue)
+    {
+        FieldDefinitionID = fieldDefinitionID;
+        FieldDefinitionValue = fieldDefinitionDataValue;
+    }
+}
