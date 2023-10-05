43ed61dd5ae63ba7062008f673e3c170f9557b4d
stewartgordonsitka <stewart.gordon@sitkatech.com>
2023-10-05 08:27:40 -0700

    Probes: * adding SystemInfoController so probe hopefully passes

---
+++ Neptune.API/Controllers/SystemInfoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Neptune.API.Controllers;
+using System;
+using Neptune.API.Services;
+using Neptune.EFModels.Entities;
+using Neptune.Models.DataTransferObjects;
+
+namespace Qanat.API.Controllers
+{
+    [ApiController]
+    public class SystemInfoController : SitkaController<SystemInfoController>
+    {
+        private readonly IOptions<NeptuneConfiguration> _neptuneConfiguration;
+
+        public SystemInfoController(NeptuneDbContext dbContext, ILogger<SystemInfoController> logger, KeystoneService keystoneService, IOptions<NeptuneConfiguration> neptuneConfiguration)
+            : base(dbContext, logger, keystoneService, neptuneConfiguration)
+        {
+            _neptuneConfiguration = neptuneConfiguration;
+        }
+
+        [HttpGet("/", Name = "GetSystemInfo")]  // MCS: the pattern seems to be to allow anonymous access to this endpoint
+        [AllowAnonymous]
+        public ActionResult<SystemInfoDto> GetSystemInfo([FromServices] IWebHostEnvironment environment)
+        {
+            SystemInfoDto systemInfo = new SystemInfoDto
+            {
+                Environment = environment.EnvironmentName,
+                CurrentTimeUTC = DateTime.UtcNow.ToString("o"),
+            };
+
+            return Ok(systemInfo);
+        }
+
+    }
+}
---
+++ Neptune.Models/DataTransferObjects/SystemInfoDto.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System;
+using System.Reflection;
+
+namespace Neptune.Models.DataTransferObjects
+{
+    public class SystemInfoDto
+    {
+        public string Environment { get; set; }
+        public string CurrentTimeUTC { get; set; }
+        public string Application => Assembly.GetEntryAssembly().GetName().Name;
+        public string FullInformationalVersion { get; set; }
+        public string PodName { get; set; }
+        public string Version { get; set; }
+        public DateTime CompilationDateTime { get; set; }
+
+        public SystemInfoDto()
+        {
+            FullInformationalVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+            var assemblyVersion = Assembly.GetEntryAssembly()!.GetName().Version;
+            Version = $"{assemblyVersion!.Major}.{assemblyVersion!.Minor}.{assemblyVersion!.Build}";
+
+            var localAssemblyPathString = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
+            var fileInfo = new FileInfo(localAssemblyPathString);
+            CompilationDateTime = fileInfo.LastWriteTime;
+        }
+    }
+}
