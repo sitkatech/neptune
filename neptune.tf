d91cab17cf74ecf5b3a1c7e3be8871d92f5cce50
raynardlee <raynardlee@yahoo.com>
2023-10-06 14:13:19 -0700

    trying removing the liveness and ready probes

--- neptune.tf
+++ neptune.tf
@@ -757,38 +757,3 @@ resource "datadog_synthetics_test" "test_geoserver" {
 
   status = "live"
 }
-
-resource "datadog_synthetics_test" "test_nereid" {
-  type    = "api"
-  subtype = "http"
-  request_definition {
-    method = "GET"
-    url    = "https://${var.domainNereid}"
-  }
-  request_headers = {
-    Content-Type   = "application/json"
-  }
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = "200"
-  }
-  locations = ["aws:us-west-1","aws:us-east-1"]
-  options_list {
-    tick_every = 900
-
-    retry {
-      count    = 2
-      interval = 30000
-    }
-
-    monitor_options {
-      renotify_interval = 120
-    }
-  }
-  name    = "${var.environment} - ${var.domainNereid} Nereid test"
-  message = "Notify @rlee@esassoc.com @sgordon@esassoc.com"
-  tags    = ["env:${var.environment}", "managed:terraformed", "team:h2o"]
-
-  status = "live"
-}
