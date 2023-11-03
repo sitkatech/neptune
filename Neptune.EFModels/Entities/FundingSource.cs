19bc0d353532c05a3d1bfd2488278cbb70bc60b7
liz-arikawa-esa <larikawa@esassoc.com>
2023-11-03 14:19:22 -0700

    add deletfull for funding source

--- Neptune.EFModels/Entities/FundingSource.cs
+++ Neptune.EFModels/Entities/FundingSource.cs
@@ -1,4 +1,6 @@
-﻿namespace Neptune.EFModels.Entities
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Neptune.EFModels.Entities
 {
     public partial class FundingSource
     {
@@ -11,6 +13,12 @@
         public List<TreatmentBMP> GetAssociatedTreatmentBMPs()
         {
             return FundingEventFundingSources.Select(x => x.FundingEvent.TreatmentBMP).Distinct().ToList();
+        }
+
+        public async Task DeleteFull(NeptuneDbContext dbContext)
+        {
+            await dbContext.FundingEventFundingSources.Where(x => x.FundingSourceID == FundingSourceID).ExecuteDeleteAsync();
+            await dbContext.FundingSources.Where(x => x.FundingSourceID == FundingSourceID).ExecuteDeleteAsync();
         }
     }
 }
