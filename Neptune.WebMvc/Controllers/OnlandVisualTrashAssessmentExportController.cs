bfcfc567c5868a0ac45fb7ea7d9cd96afa20b537
liz-arikawa-esa <larikawa@esassoc.com>
2024-12-23 13:23:16 -0800

    add endpoint to get the ovta area data as a gdb

--- Neptune.WebMvc/Controllers/OnlandVisualTrashAssessmentExportController.cs
+++ Neptune.WebMvc/Controllers/OnlandVisualTrashAssessmentExportController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Neptune.Common.GeoSpatial;
+using Neptune.Common.Services.GDAL;
 using Neptune.EFModels.Entities;
 using Neptune.WebMvc.Common;
 using Neptune.WebMvc.Security;
-using Neptune.WebMvc.Views.LandUseBlockGeometry;
 using Neptune.WebMvc.Views.OnlandVisualTrashAssessmentExport;
+using NetTopologySuite.Features;
 
 namespace Neptune.WebMvc.Controllers
 {
@@ -12,8 +15,10 @@ namespace Neptune.WebMvc.Controllers
     //[Route("[area]/[controller]/[action]", Name = "[area]_[controller]_[action]")]
     public class OnlandVisualTrashAssessmentExportController : NeptuneBaseController<OnlandVisualTrashAssessmentExportController>
     {
-        public OnlandVisualTrashAssessmentExportController(NeptuneDbContext dbContext, ILogger<OnlandVisualTrashAssessmentExportController> logger, IOptions<WebConfiguration> webConfiguration, LinkGenerator linkGenerator) : base(dbContext, logger, linkGenerator, webConfiguration)
+        private readonly GDALAPIService _gdalApiService;
+        public OnlandVisualTrashAssessmentExportController(NeptuneDbContext dbContext, ILogger<OnlandVisualTrashAssessmentExportController> logger, IOptions<WebConfiguration> webConfiguration, LinkGenerator linkGenerator, GDALAPIService gdalApiService) : base(dbContext, logger, linkGenerator, webConfiguration)
         {
+            _gdalApiService=gdalApiService;
         }
 
         [HttpGet]
@@ -26,9 +31,89 @@ namespace Neptune.WebMvc.Controllers
 
         [HttpPost]
         [NeptuneViewAndRequiresJurisdictionsFeature]
-        public ActionResult ExportAssessmentGeospatialData(ExportAssessmentGeospatialDataViewModel viewModel)
+        public async Task<ActionResult> ExportAssessmentGeospatialData(ExportAssessmentGeospatialDataViewModel viewModel)
         {
-            return ViewExportAssessmentGeospatialData(viewModel);
+            var stormwaterJurisdictionName = _dbContext.StormwaterJurisdictions.Include(x => x.Organization)
+                .Single(x => x.StormwaterJurisdictionID == viewModel.StormwaterJurisdictionID)
+                .GetOrganizationDisplayName();
+            var areaFeatureCollection = new FeatureCollection();
+            var transectLineFeatureCollection = new FeatureCollection();
+            var observationPointFeatureCollection = new FeatureCollection();
+            var areas = _dbContext.OnlandVisualTrashAssessmentAreas
+                .Include(x => x.OnlandVisualTrashAssessment)
+                .Where(x => x.StormwaterJurisdictionID == viewModel.StormwaterJurisdictionID).ToList();
+            var observations = _dbContext.OnlandVisualTrashAssessmentObservations
+                .Include(x => x.OnlandVisualTrashAssessmentObservationPhotos).ThenInclude(
+                    onlandVisualTrashAssessmentObservationPhoto =>
+                        onlandVisualTrashAssessmentObservationPhoto.FileResource)
+                .Include(x => x.OnlandVisualTrashAssessment)
+                .ThenInclude(x => x.OnlandVisualTrashAssessmentArea)
+                .Where(x => x.OnlandVisualTrashAssessment.StormwaterJurisdictionID == viewModel.StormwaterJurisdictionID).ToList();
+
+            foreach (var area in areas)
+            {
+                var attributesTable = new AttributesTable
+                {
+                    { "OVTAAreaName", area.OnlandVisualTrashAssessmentAreaName },
+                    { "Description", area.AssessmentAreaDescription ?? null },
+                    { "CreatedOn", area.OnlandVisualTrashAssessment?.CreatedDate }
+                };
+                areaFeatureCollection.Add(new Feature(area.OnlandVisualTrashAssessmentAreaGeometry4326, attributesTable));
+
+                var attributeTable2 = new AttributesTable()
+                {
+                    { "OVTAAreaName", area.OnlandVisualTrashAssessmentAreaName },
+                    { "JurisID", area.StormwaterJurisdictionID }
+                };
+                transectLineFeatureCollection.Add(new Feature(area.TransectLine4326, attributeTable2));
+            }
+
+            foreach (var observation in observations)
+            {
+                var fileResourceGuid = observation.OnlandVisualTrashAssessmentObservationPhotos.FirstOrDefault()?.FileResource.FileResourceGUID;
+                var attributeTable3 = new AttributesTable()
+                {
+                    { "OVTAAreaName", observation.OnlandVisualTrashAssessment.OnlandVisualTrashAssessmentArea?.OnlandVisualTrashAssessmentAreaName },
+                    { "AssessmentID", observation.OnlandVisualTrashAssessment?.OnlandVisualTrashAssessmentID },
+                    { "Note", observation.Note },
+                    { "JurisID", observation.OnlandVisualTrashAssessment?.StormwaterJurisdictionID },
+                    { "JurisName", stormwaterJurisdictionName },
+                    { "Score", observation.OnlandVisualTrashAssessment?.OnlandVisualTrashAssessmentScore.OnlandVisualTrashAssessmentScoreDisplayName },
+                    { "CompletedDate", observation.OnlandVisualTrashAssessment?.CompletedDate },
+                    { "PhotoUrl", fileResourceGuid != null ? $"/FileResource/DisplayResource/{fileResourceGuid}" : null },
+                };
+                observationPointFeatureCollection.Add(new Feature(observations.FirstOrDefault(x => x.OnlandVisualTrashAssessmentID == observation.OnlandVisualTrashAssessment?.OnlandVisualTrashAssessmentID)?.LocationPoint4326, attributeTable3));
+            }
+
+            var gdbInput = new GdbInput()
+            {
+                FileContents = GeoJsonSerializer.SerializeToByteArray(areaFeatureCollection, GeoJsonSerializer.DefaultSerializerOptions),
+                LayerName = "ovta-areas",
+                CoordinateSystemID = Proj4NetHelper.WEB_MERCATOR,
+                GeometryTypeName = "POLYGON",
+            };
+            var gdbInput2 = new GdbInput()
+            {
+                FileContents = GeoJsonSerializer.SerializeToByteArray(transectLineFeatureCollection, GeoJsonSerializer.DefaultSerializerOptions),
+                LayerName = "transect-lines",
+                CoordinateSystemID = Proj4NetHelper.WEB_MERCATOR,
+                GeometryTypeName = "LINESTRING",
+            };
+            var gdbInput3 = new GdbInput()
+            {
+                FileContents = GeoJsonSerializer.SerializeToByteArray(observationPointFeatureCollection, GeoJsonSerializer.DefaultSerializerOptions),
+                LayerName = "observation-point",
+                CoordinateSystemID = Proj4NetHelper.WEB_MERCATOR,
+                GeometryTypeName = "POINT",
+            };
+            var bytes = await _gdalApiService.Ogr2OgrInputToGdbAsZip(new GdbInputsToGdbRequestDto()
+            {
+                GdbInputs = new List<GdbInput> { gdbInput, gdbInput2, gdbInput3 },
+                GdbName = $"test"
+            });
+
+
+            return File(bytes, "application/zip", $"test.gdb.zip");
         }
 
         private ViewResult ViewExportAssessmentGeospatialData(ExportAssessmentGeospatialDataViewModel viewModel)
