f166fade018f4b0057ccc88371f5b34c3825a8a5
raynardlee <raynardlee@yahoo.com>
2025-01-13 22:00:08 -0800

    NPT-910: removing unused files

--- Neptune.Web/src/app/shared/models/validation-error.ts
+++
@@ -1,7 +0,0 @@
-export class ValidationError {
-    constructor(public entity?: string,
-                public invalidValue?: string,
-                public property?: string,
-                public message?: string) {
-    }
-}
