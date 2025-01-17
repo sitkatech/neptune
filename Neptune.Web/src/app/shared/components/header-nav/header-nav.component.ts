868759f84ea47e1c0eef08b52865e151e20d6c70
raynardlee <raynardlee@yahoo.com>
2025-01-16 21:25:00 -0800

    NPT-912: switching to use currentUser$ observable instead of a watch user subscription

--- Neptune.Web/src/app/shared/components/header-nav/header-nav.component.ts
+++ Neptune.Web/src/app/shared/components/header-nav/header-nav.component.ts
@@ -1,57 +1,39 @@
-import { Component, OnInit, HostListener, ChangeDetectorRef, OnDestroy, Input } from "@angular/core";
+import { Component, OnInit, HostListener, Input } from "@angular/core";
 import { AuthenticationService } from "src/app/services/authentication.service";
 import { environment } from "src/environments/environment";
 import { PersonDto } from "src/app/shared/generated/model/person-dto";
-import { NgIf } from "@angular/common";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { AsyncPipe, NgIf } from "@angular/common";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 import { DropdownToggleDirective } from "src/app/shared/directives/dropdown-toggle.directive";
 import { IconComponent } from "src/app/shared/components/icon/icon.component";
+import { Observable } from "rxjs";
 
 @Component({
     selector: "header-nav",
     templateUrl: "./header-nav.component.html",
     styleUrls: ["./header-nav.component.scss"],
     standalone: true,
-    imports: [RouterLink, RouterLinkActive, NgIf, DropdownToggleDirective, IconComponent],
+    imports: [RouterLink, RouterLinkActive, NgIf, AsyncPipe, DropdownToggleDirective, IconComponent],
 })
-export class HeaderNavComponent implements OnInit, OnDestroy {
+export class HeaderNavComponent implements OnInit {
     @Input() moduleTitle: string;
-    private watchUserChangeSubscription: any;
-    private currentUser: PersonDto;
+    public currentUser$: Observable<PersonDto>;
 
     public windowWidth: number;
-
-    public showCurrentPageHeader: boolean = true;
 
     @HostListener("window:resize", ["$event"])
     resize() {
         this.windowWidth = window.innerWidth;
     }
 
-    constructor(private authenticationService: AuthenticationService, private cdr: ChangeDetectorRef) {}
+    constructor(private authenticationService: AuthenticationService) {}
 
     ngOnInit() {
-        this.watchUserChangeSubscription = this.authenticationService.currentUserSetObservable.subscribe((currentUser) => {
-            this.currentUser = currentUser;
-        });
-    }
-
-    ngOnDestroy() {
-        this.watchUserChangeSubscription.unsubscribe();
-
-        this.cdr.detach();
+        this.currentUser$ = this.authenticationService.getCurrentUser();
     }
 
     public isAuthenticated(): boolean {
         return this.authenticationService.isAuthenticated();
-    }
-
-    public isAdministrator(): boolean {
-        return this.authenticationService.isUserAnAdministrator(this.currentUser);
-    }
-
-    public getUserName() {
-        return this.currentUser ? this.currentUser.FirstName + " " + this.currentUser.LastName : null;
     }
 
     public login(): void {
@@ -60,10 +42,6 @@ export class HeaderNavComponent implements OnInit, OnDestroy {
 
     public logout(): void {
         this.authenticationService.logout();
-
-        setTimeout(() => {
-            this.cdr.detectChanges();
-        });
     }
 
     public ocStormwaterToolsMainUrl(): string {
