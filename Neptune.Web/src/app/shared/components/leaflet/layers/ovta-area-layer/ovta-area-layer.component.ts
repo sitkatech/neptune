14adb68543393d9a6d05c572bb3fd1c99639b015
liz-arikawa-esa <larikawa@esassoc.com>
2025-02-10 13:46:45 -0800

    add 2 way binding for ovta area index page

--- Neptune.Web/src/app/shared/components/leaflet/layers/ovta-area-layer/ovta-area-layer.component.ts
+++ Neptune.Web/src/app/shared/components/leaflet/layers/ovta-area-layer/ovta-area-layer.component.ts
@@ -14,25 +14,119 @@ import { GroupByPipe } from "src/app/shared/pipes/group-by.pipe";
     styleUrl: "./ovta-area-layer.component.scss",
 })
 export class OvtaAreaLayerComponent extends MapLayerBase implements OnChanges, AfterViewInit {
-    constructor() {
+    @Input() selectedOVTAAreaID: number;
+    @Input() ovtaAreaID: number;
+
+    @Output() layerBoundsCalculated = new EventEmitter();
+    @Output() ovtaAreaSelected = new EventEmitter<number>();
+
+    public isLoading: boolean = false;
+
+    public layer: L.featureGroup;
+
+    private defaultStyle = {
+        color: "#3388ff",
+        weight: 2,
+        opacity: 0.65,
+        fillOpacity: 0.1,
+    };
+
+    private highlightStyle = {
+        color: "#fcfc12",
+        weight: 2,
+        opacity: 0.65,
+        fillOpacity: 0.1,
+    };
+
+    constructor(private wfsService: WfsService, private groupByPipe: GroupByPipe) {
         super();
     }
-    @Input() ovtaAreaID: number;
-    public wmsOptions: L.WMSOptions;
-    public layer;
+
+    ngOnChanges(changes: any): void {
+        if (changes.selectedOVTAAreaID) {
+            if (changes.selectedOVTAAreaID.previousValue == changes.selectedOVTAAreaID.currentValue) return;
+            this.selectedOVTAAreaID = changes.selectedOVTAAreaID.currentValue;
+            this.highlightSelectedOVTAArea();
+        } else if (Object.values(changes).some((x: SimpleChange) => x.firstChange === false)) {
+            this.updateLayer();
+        }
+    }
 
     ngAfterViewInit(): void {
-        this.wmsOptions = {
-            layers: "OCStormwater:OnlandVisualTrashAssessmentAreas",
-            transparent: true,
-            format: "image/png",
-            tiled: true,
-        };
+        this.setupLayer();
+        this.updateLayer();
+    }
+
+    private updateLayer() {
+        this.isLoading = true;
+        this.layer.clearLayers();
+
+        this.addOVTAAreasToLayer();
+
+        this.layer.addTo(this.map);
+        this.isLoading = false;
+    }
+
+    private addOVTAAreasToLayer() {
+        let cql_filter = ``;
         if (this.ovtaAreaID) {
-            this.wmsOptions.cql_filter = `OnlandVisualTrashAssessmentAreaID = ${this.ovtaAreaID}`;
+            cql_filter += `OVTAAreaID == ${this.ovtaAreaID}`;
         }
 
-        this.layer = L.tileLayer.wms(environment.geoserverMapServiceUrl + "/wms?", this.wmsOptions);
+        this.wfsService.getGeoserverWFSLayer("OCStormwater:AssessmentAreaExport", cql_filter, "OVTAAreaID").subscribe((response) => {
+            if (response.length == 0) return;
+
+            const featuresGroupedByOVTAAreaID = this.groupByPipe.transform(response, "properties.OVTAAreaID");
+
+            Object.keys(featuresGroupedByOVTAAreaID).forEach((ovtaAreaID) => {
+                const geoJson = L.geoJSON(featuresGroupedByOVTAAreaID[ovtaAreaID], {
+                    style: this.defaultStyle,
+                });
+                geoJson.on("mouseover", (e) => {
+                    geoJson.setStyle({ fillOpacity: 0.5 });
+                });
+                geoJson.on("mouseout", (e) => {
+                    geoJson.setStyle({ fillOpacity: 0.1 });
+                });
+
+                geoJson.on("click", (e) => {
+                    this.onOVTAAreaSelected(Number(ovtaAreaID));
+                });
+
+                geoJson.addTo(this.layer);
+            });
+
+            const bounds = this.layer.getBounds();
+            this.map.fitBounds(bounds);
+            this.layerBoundsCalculated.emit(bounds);
+        });
+    }
+
+    private onOVTAAreaSelected(ovtaAreaID: number) {
+        this.selectedOVTAAreaID = ovtaAreaID;
+        this.highlightSelectedOVTAArea();
+
+        this.ovtaAreaSelected.emit(ovtaAreaID);
+    }
+
+    private highlightSelectedOVTAArea() {
+        // clear styles
+        this.layer.setStyle(this.defaultStyle);
+
+        this.layer.eachLayer((layer) => {
+            // skip if well layer
+            if (layer.options?.icon) return;
+
+            const geoJsonLayers = layer.getLayers();
+            if (geoJsonLayers[0].feature.properties.OVTAAreaID == this.selectedOVTAAreaID) {
+                layer.setStyle(this.highlightStyle);
+                this.map.fitBounds(layer.getBounds());
+            }
+        });
+    }
+
+    private setupLayer() {
+        this.layer = L.geoJSON();
         this.initLayer();
     }
 }
--- Neptune.Web/src/app/shared/services/wfs.service.ts
+++ Neptune.Web/src/app/shared/services/wfs.service.ts
@@ -1,47 +1,70 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {FeatureCollection} from "geojson";
-import {Observable} from "rxjs";
-import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { FeatureCollection } from "geojson";
+import { Observable, map } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root",
 })
 export class WfsService {
-    constructor(
-        private http: HttpClient,
-    ) {
-    }
+    constructor(private http: HttpClient) {}
 
     public getOCTAPrioritizationMetricsByCoordinate(longitude: number, latitude: number): Observable<FeatureCollection> {
         const url: string = `${environment.geoserverMapServiceUrl}/wms`;
         return this.http.get<FeatureCollection>(url, {
             params: {
-                service: 'WFS',
-                version: '2.0',
-                request: 'GetFeature',
-                outputFormat: 'application/json',
-                SrsName: 'EPSG:4326',
-                typeName: 'Neptune:OCTAPrioritization',
-                cql_filter: `intersects(OCTAPrioritizationGeometry, POINT(${latitude} ${longitude}))`
-            }
+                service: "WFS",
+                version: "2.0",
+                request: "GetFeature",
+                outputFormat: "application/json",
+                SrsName: "EPSG:4326",
+                typeName: "Neptune:OCTAPrioritization",
+                cql_filter: `intersects(OCTAPrioritizationGeometry, POINT(${latitude} ${longitude}))`,
+            },
         });
     }
 
     public getRegionalSubbasins(): Observable<FeatureCollection> {
-        var owsrootUrl = environment.geoserverMapServiceUrl + '/ows';
+        var owsrootUrl = environment.geoserverMapServiceUrl + "/ows";
 
         var defaultParameters = {
-            service : 'WFS',
-            version : '2.0',
-            request : 'GetFeature',
-            typeName : 'OCStormwater:RegionalSubbasins',
-            outputFormat : 'application/json',
+            service: "WFS",
+            version: "2.0",
+            request: "GetFeature",
+            typeName: "OCStormwater:RegionalSubbasins",
+            outputFormat: "application/json",
             // format_options : 'callback:getJson',
-            SrsName : 'EPSG:4326'
+            SrsName: "EPSG:4326",
         };
         return this.http.get<FeatureCollection>(owsrootUrl, {
-            params: defaultParameters
+            params: defaultParameters,
         });
+    }
+
+    public getGeoserverWFSLayer(layer: string, cqlFilter: string, valueReference: string): Observable<number[]> {
+        const cqlFilters = [];
+
+        if (cqlFilter) {
+            cqlFilters.push(`${cqlFilter}`);
+        }
+        const cqlFiltersCombined = cqlFilters.join(" and ");
+
+        const url: string = `${environment.geoserverMapServiceUrl}/ows`;
+        const wfsParams = new HttpParams()
+            .set("responseType", "json")
+            .set("service", "wfs")
+            .set("version", "2.0")
+            .set("request", "GetFeature")
+            .set("SrsName", "EPSG:4326")
+            .set("typeName", layer)
+            .set("outputFormat", "application/json")
+            .set("valueReference", valueReference)
+            .set("cql_filter", cqlFiltersCombined);
+        return this.http.post(url, wfsParams).pipe(
+            map((rawData: any) => {
+                return rawData.features;
+            })
+        );
     }
 }
