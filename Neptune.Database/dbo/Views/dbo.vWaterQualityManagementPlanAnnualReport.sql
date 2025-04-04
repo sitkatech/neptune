661925807c9019f4979159da3a66c5f944423be3
Shannon Bulloch <sbulloch@esassoc.com>
2025-04-04 10:48:48 -0700

    NPT-884: change grid columns for the Post Construction Inspection And Verification Grid. Removr PrimaryKey column from view and move logic to calculate reporting year start/end to helper functions.

--- Neptune.Database/dbo/Views/dbo.vWaterQualityManagementPlanAnnualReport.sql
+++ Neptune.Database/dbo/Views/dbo.vWaterQualityManagementPlanAnnualReport.sql
@@ -1,66 +1,60 @@
-create view dbo.vWaterQualityManagementPlanAnnualReport
+create view [dbo].[vWaterQualityManagementPlanAnnualReport]
 as
 
 select 
-	   concat(wqmp.WaterQualityManagementPlanID,'_', wqmpv.WaterQualityManagementPlanVerifyID, '_', fv.FieldVisitID) as PrimaryKey,
 	   wqmp.WaterQualityManagementPlanID,
        wqmp.WaterQualityManagementPlanName,
        wqmp.WaterQualityManagementPlanStatusID,
        wqmp.StormwaterJurisdictionID,
        sj.StormwaterJurisdictionPublicWQMPVisibilityTypeID,
        wqmp.ApprovalDate,
-	   bmpcount.TreatmentBMPCount,
-	   qbmpcount.QuickBMPCount,
-	   qbmpcount.QuickBMPNumberOfIndividualBMPs,
        wqmpv.WaterQualityManagementPlanVerifyID,
-       wqmpv.IsDraft as WaterQualityManagementPlanVerifyIsDraft,
+	   wqmpv.WaterQualityManagementPlanVerifyStatusID,
        wqmpv.VerificationDate as WaterQualityManagementPlanVerifyVerificationDate,
-	   vbmpnotes.WaterQualityManagementPlanVerifyTreatmentBMPNotes,
-	   vqnotes.WaterQualityManagementPlanVerifyQuickBMPNotes,
-	   fv.FieldVisitID,
-       fv.FieldVisitStatusID,
-       fv.VisitDate as FieldVisitDate
+	   wqmpv.EnforcementOrFollowupActions,
+	   vbmp.WaterQualityManagementPlanVerifyTreatmentBMPCount,
+	   vbmp.WaterQualityManagementPlanVerifyTreatmentBMPIsAdequateCount,
+	   vbmp.WaterQualityManagementPlanVerifyTreatmentBMPIsDeficientCount,
+	   vbmp.WaterQualityManagementPlanVerifyTreatmentBMPNotes,
+	   vqbmp.WaterQualityManagementPlanVerifyQuickBMPCount, 
+	   vqbmpadequate.WaterQualityManagementPlanVerifyQuickBMPIsAdequateCount,
+	   vqbmp.WaterQualityManagementPlanVerifyQuickBMPCount - vqbmpadequate.WaterQualityManagementPlanVerifyQuickBMPIsAdequateCount as WaterQualityManagementPlanVerifyQuickBMPIsDeficient,
+	   vqbmp.WaterQualityManagementPlanVerifyQuickBMPNotes
 from dbo.WaterQualityManagementPlan wqmp
 join dbo.StormwaterJurisdiction sj on wqmp.StormwaterJurisdictionID = sj.StormwaterJurisdictionID
-left join dbo.WaterQualityManagementPlanVerify wqmpv on wqmp.WaterQualityManagementPlanID = wqmpv.WaterQualityManagementPlanID
-
-left join dbo.TreatmentBMP bmp on wqmp.WaterQualityManagementPlanID = bmp.WaterQualityManagementPlanID
-left join dbo.FieldVisit fv on bmp.TreatmentBMPID = fv.TreatmentBMPID
-left join dbo.FieldVisitStatus fvs on fv.FieldVisitStatusID = fvs.FieldVisitStatusID
-
-left join (
-	select count(*) as TreatmentBMPCount
-	,WaterQualityManagementPlanID
-	from dbo.TreatmentBMP 
-	group by WaterQualityManagementPlanID
-			
-) bmpcount on bmpcount.WaterQualityManagementPlanID = wqmp.WaterQualityManagementPlanID
-
-left join (
-	select sum(NumberOfIndividualBMPs) as QuickBMPNumberOfIndividualBMPs
-	,count(*) as QuickBMPCount
-	,WaterQualityManagementPlanID
-	from dbo.QuickBMP 
-	group by WaterQualityManagementPlanID
-			
-) qbmpcount on qbmpcount.WaterQualityManagementPlanID = wqmp.WaterQualityManagementPlanID
+join dbo.WaterQualityManagementPlanVerify wqmpv on wqmp.WaterQualityManagementPlanID = wqmpv.WaterQualityManagementPlanID
 
 left join (
 	select
 		STRING_AGG(WaterQualityManagementPlanVerifyTreatmentBMPNote, '; ') as WaterQualityManagementPlanVerifyTreatmentBMPNotes,
+		count(*) as WaterQualityManagementPlanVerifyTreatmentBMPCount,
+		count(IsAdequate) as WaterQualityManagementPlanVerifyTreatmentBMPIsAdequateCount,
+		count(*) - count(IsAdequate) as WaterQualityManagementPlanVerifyTreatmentBMPIsDeficientCount,
 		WaterQualityManagementPlanVerifyID
 	from dbo.WaterQualityManagementPlanVerifyTreatmentBMP vbmp
 	group by WaterQualityManagementPlanVerifyID
-) vbmpnotes on vbmpnotes.WaterQualityManagementPlanVerifyID = wqmpv.WaterQualityManagementPlanVerifyID
+) vbmp on vbmp.WaterQualityManagementPlanVerifyID = wqmpv.WaterQualityManagementPlanVerifyID
 
 left join (
 	select
 		STRING_AGG(WaterQualityManagementPlanVerifyQuickBMPNote, '; ') as WaterQualityManagementPlanVerifyQuickBMPNotes,
+		sum(qbmp.NumberOfIndividualBMPs) as WaterQualityManagementPlanVerifyQuickBMPCount,
 		WaterQualityManagementPlanVerifyID
 	from dbo.WaterQualityManagementPlanVerifyQuickBMP wqmpqv
+	join dbo.QuickBMP qbmp on wqmpqv.QuickBMPID = qbmp.QuickBMPID
 	group by WaterQualityManagementPlanVerifyID
-) vqnotes on vqnotes.WaterQualityManagementPlanVerifyID = wqmpv.WaterQualityManagementPlanVerifyID
+) vqbmp on vqbmp.WaterQualityManagementPlanVerifyID = wqmpv.WaterQualityManagementPlanVerifyID
 
-where  wqmp.ApprovalDate is not null and (wqmpv.IsDraft is null or wqmpv.IsDraft = 0) and (fv.FieldVisitStatusID is null or fv.FieldVisitStatusID = 2)
+left join (
+	select
+		sum(qbmp.NumberOfIndividualBMPs) as WaterQualityManagementPlanVerifyQuickBMPIsAdequateCount,
+		WaterQualityManagementPlanVerifyID
+	from dbo.WaterQualityManagementPlanVerifyQuickBMP wqmpqv
+	join dbo.QuickBMP qbmp on wqmpqv.QuickBMPID = qbmp.QuickBMPID
+	where wqmpqv.IsAdequate = 1
+	group by WaterQualityManagementPlanVerifyID
+) vqbmpadequate on vqbmpadequate.WaterQualityManagementPlanVerifyID = wqmpv.WaterQualityManagementPlanVerifyID
+
+where  wqmp.ApprovalDate is not null and wqmpv.IsDraft = 0
 
 GO
