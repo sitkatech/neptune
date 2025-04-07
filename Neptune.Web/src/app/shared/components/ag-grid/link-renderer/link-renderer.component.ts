e6582393f7986bd9d98238d7dcaeb359b0e430eb
Shannon Bulloch <sbulloch@esassoc.com>
2025-04-07 10:58:39 -0700

    NPT-993: Created links from SPA grid to MVC pages via href in link-renderer.component; made WQMP, OVTA Area, and Treatment BMP columns links. Do not filter TreatmentBMPs to IsAnalyzedInModelingModule only for the Trash Module map.

--- Neptune.Web/src/app/shared/components/ag-grid/link-renderer/link-renderer.component.ts
+++ Neptune.Web/src/app/shared/components/ag-grid/link-renderer/link-renderer.component.ts
@@ -1,14 +1,13 @@
 import { Component } from "@angular/core";
 import { AgRendererComponent } from "ag-grid-angular";
 import { RouterLink } from "@angular/router";
-import { NgIf } from "@angular/common";
 
 @Component({
     selector: "qanat-link-renderer",
     templateUrl: "./link-renderer.component.html",
     styleUrls: ["./link-renderer.component.scss"],
     standalone: true,
-    imports: [NgIf, RouterLink],
+    imports: [RouterLink],
 })
 export class LinkRendererComponent implements AgRendererComponent {
     params: {
@@ -16,8 +15,9 @@ export class LinkRendererComponent implements AgRendererComponent {
             LinkDisplay: string;
             LinkValue: string;
             queryParams?: {};
+            href?: string;
         };
-        inRouterLink: string;
+        inRouterLink?: string;
         cssClasses?: string;
     };
 
