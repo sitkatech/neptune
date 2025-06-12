7f3de3f8115c8df9f39d70b88b5ca20e999994ba
Mack Peters <mpeters@esassoc.com>
2025-06-12 08:41:49 -0700

    Add stored procedure that will generate the regional subbasin network

---
+++ Neptune.Database/dbo/Procs/dbo.pRegionalSubbasinGenerateNetwork.sql
@@ -0,0 +1,63 @@
+Create Procedure dbo.pRegionalSubbasinGenerateNetwork
+
+(
+    @regionalSubbasinID int
+)
+
+As
+        drop table if exists #rsbIDs
+        create table #rsbIDs
+        (
+            RegionalSubbasinID int not null primary key,
+            OCSurveyCatchmentID int not null,
+            OCSurveyDownstreamCatchmentID int null,
+            --This is the depth of the Regional Subbasin in the network, where 0 is the root Regional Subbasin, positive numbers are upstream and negative numbers are downstream
+            Depth int not null
+        )
+
+        insert into #rsbIDs(RegionalSubbasinID, OCSurveyCatchmentID, OCSurveyDownstreamCatchmentID, Depth)
+        select n.RegionalSubbasinID, n.OCSurveyCatchmentID, n.OCSurveyDownstreamCatchmentID, 0 as Depth
+        from dbo.RegionalSubbasin n
+        where n.RegionalSubbasinID = @regionalSubbasinID
+
+        -- Find all downstream Regional Subbasins
+        declare @depth int = 0
+        while(exists(select 1 from dbo.RegionalSubbasin n join #rsbIDs s on n.OCSurveyDownstreamCatchmentID = s.OCSurveyCatchmentID and s.Depth = @depth left join #rsbIDs s2 on n.RegionalSubbasinID = s2.RegionalSubbasinID and s2.RegionalSubbasinID is null))
+        begin
+                insert into #rsbIDs(RegionalSubbasinID, OCSurveyCatchmentID, OCSurveyDownstreamCatchmentID, Depth)
+                select a.RegionalSubbasinID, a.OCSurveyCatchmentID, a.OCSurveyDownstreamCatchmentID, a.Depth
+                from
+                (
+                    select distinct n.RegionalSubbasinID, n.OCSurveyCatchmentID, n.OCSurveyDownstreamCatchmentID, s.Depth - 1 as Depth
+                    from dbo.RegionalSubbasin n
+                    join #rsbIDs s on n.OCSurveyDownstreamCatchmentID = s.OCSurveyCatchmentID and s.Depth = @depth
+                ) a
+                left join #rsbIDs p on a.RegionalSubbasinID = p.RegionalSubbasinID
+                where p.RegionalSubbasinID is null
+            set @depth = @depth - 1
+        end
+
+        -- Find all upstream Regional Subbasins
+        set @depth = 0
+        while(exists(select 1 from dbo.RegionalSubbasin n join #rsbIDs s on n.OCSurveyCatchmentID = s.OCSurveyDownstreamCatchmentID and s.Depth = @depth left join #rsbIDs s2 on n.RegionalSubbasinID = s2.RegionalSubbasinID and s2.RegionalSubbasinID is null))
+        begin
+                insert into #rsbIDs(RegionalSubbasinID, OCSurveyCatchmentID, OCSurveyDownstreamCatchmentID, Depth)
+                select a.RegionalSubbasinID, a.OCSurveyCatchmentID, a.OCSurveyDownstreamCatchmentID, a.Depth
+                from
+                (
+                    select distinct n.RegionalSubbasinID, n.OCSurveyCatchmentID, n.OCSurveyDownstreamCatchmentID, s.Depth + 1 as Depth
+                    from dbo.RegionalSubbasin n
+                    join #rsbIDs s on n.OCSurveyCatchmentID = s.OCSurveyDownstreamCatchmentID and s.Depth = @depth
+                ) a
+                left join #rsbIDs p on a.RegionalSubbasinID = p.RegionalSubbasinID
+                where p.RegionalSubbasinID is null
+            set @depth = @depth + 1
+        end
+        
+        select @regionalSubbasinID, #rsbIDs.OCSurveyCatchmentID, #rsbIDs.OCSurveyDownstreamCatchmentID, Depth, geometry::STGeomFromText('LINESTRING(' + CAST(rsb.CatchmentGeometry4326.STCentroid().STX AS VARCHAR(20)) + ' ' + CAST(rsb.CatchmentGeometry4326.STCentroid().STY AS VARCHAR(20)) + ', ' + CAST(rsbu.CatchmentGeometry4326.STCentroid().STX AS VARCHAR(20)) + ' ' + CAST(rsbu.CatchmentGeometry4326.STCentroid().STY AS VARCHAR(20)) + ')', 4326)
+        from #rsbIDs
+        join dbo.RegionalSubbasin rsb on #rsbIDs.OCSurveyCatchmentID = rsb.OCSurveyCatchmentID
+        left join dbo.RegionalSubbasin rsbu on #rsbIDs.OCSurveyDownstreamCatchmentID = rsbu.OCSurveyCatchmentID
+        order by Depth, #rsbIDs.RegionalSubbasinID
+
+GO
