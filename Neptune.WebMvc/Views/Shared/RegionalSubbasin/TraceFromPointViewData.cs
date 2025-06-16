08477192e0d920501fd24a830ff5041f0a6b7d24
Mack Peters <mpeters@esassoc.com>
2025-06-16 16:07:24 -0700

    Start partial method of adding control to the map

---
+++ Neptune.WebMvc/Views/Shared/RegionalSubbasin/TraceFromPointViewData.cs
@@ -0,0 +1,7 @@
+﻿namespace Neptune.WebMvc.Views.Shared.RegionalSubbasin
+{
+    public class TraceFromPointViewData(string mapDivID)
+    {
+        public readonly string MapDivID = mapDivID;
+    }
+}
