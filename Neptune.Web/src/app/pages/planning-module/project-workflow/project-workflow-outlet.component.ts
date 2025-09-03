80ed90a51e14916dc253c0406dd386d80d4f5c00
Mack Peters <mpeters@esassoc.com>
2025-09-03 11:44:51 -0700

    NPT-974 Trying to make project match ovta -Because for whatever reason, ovta can have draw control but planning can't. Change the location of the files so maybe a decrease in depth will help -Remove layeredgroupcontrol from angular.json scripts just to see if it's needed there

--- Neptune.Web/src/app/pages/planning-module/project-workflow/project-workflow-outlet.component.ts
+++ Neptune.Web/src/app/pages/planning-module/project-workflow/project-workflow-outlet.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ProjectUpsertDto } from "src/app/shared/generated/model/project-upsert-dto";
-import { ProjectWizardSidebarComponent } from "src/app/pages/planning-module/projects/project-wizard-sidebar/project-wizard-sidebar.component";
+import { ProjectWizardSidebarComponent } from "src/app/pages/planning-module/project-workflow/project-wizard-sidebar/project-wizard-sidebar.component";
 
 @Component({
     selector: "project-workflow-outlet",
     templateUrl: "./project-workflow-outlet.component.html",
     styleUrls: ["./project-workflow-outlet.component.scss"],
-    imports: [ProjectWizardSidebarComponent, RouterOutlet]
+    imports: [ProjectWizardSidebarComponent, RouterOutlet],
 })
 export class ProjectWorkflowOutletComponent implements OnInit {
     public projectModel: ProjectUpsertDto;
