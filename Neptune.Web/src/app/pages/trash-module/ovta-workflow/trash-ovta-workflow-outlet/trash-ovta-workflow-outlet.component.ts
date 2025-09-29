359ff53601cb67a3189004a66af7da126747fb98
raynardlee <raynardlee@yahoo.com>
2025-09-29 13:57:01 -0700

    merging trash-ovta-wizard-sidebar.component into trash-ovta-workflow-outlet.component and using withComponentInputBinding

--- Neptune.Web/src/app/pages/trash-module/ovta-workflow/trash-ovta-workflow-outlet/trash-ovta-workflow-outlet.component.ts
+++ Neptune.Web/src/app/pages/trash-module/ovta-workflow/trash-ovta-workflow-outlet/trash-ovta-workflow-outlet.component.ts
@@ -1,11 +1,62 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { TrashOvtaWizardSidebarComponent } from "../trash-ovta-wizard-sidebar/trash-ovta-wizard-sidebar.component";
+import { Component, OnInit, Input } from "@angular/core";
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
+import { Observable, tap } from "rxjs";
+import { OvtaWorkflowProgressService } from "src/app/shared/services/ovta-workflow-progress.service";
+import { AsyncPipe, DatePipe } from "@angular/common";
+import { OnlandVisualTrashAssessmentWorkflowProgressDto } from "src/app/shared/generated/model/onland-visual-trash-assessment-workflow-progress-dto";
+import { OnlandVisualTrashAssessmentStatusEnum } from "src/app/shared/generated/enum/onland-visual-trash-assessment-status-enum";
+import { ConfirmService } from "src/app/shared/services/confirm/confirm.service";
+import { OnlandVisualTrashAssessmentService } from "src/app/shared/generated/api/onland-visual-trash-assessment.service";
+import { Alert } from "src/app/shared/models/alert";
+import { AlertContext } from "src/app/shared/models/enums/alert-context.enum";
+import { AlertService } from "src/app/shared/services/alert.service";
+import { WorkflowNavComponent } from "../../../../shared/components/workflow-nav/workflow-nav.component";
+import { WorkflowNavItemComponent } from "../../../../shared/components/workflow-nav/workflow-nav-item/workflow-nav-item.component";
+import { IconComponent } from "../../../../shared/components/icon/icon.component";
 
 @Component({
     selector: "trash-ovta-workflow-outlet",
-    imports: [RouterOutlet, TrashOvtaWizardSidebarComponent],
+    imports: [RouterOutlet, RouterLink, AsyncPipe, WorkflowNavComponent, WorkflowNavItemComponent, IconComponent],
     templateUrl: "./trash-ovta-workflow-outlet.component.html",
-    styleUrl: "./trash-ovta-workflow-outlet.component.scss"
+    styleUrl: "./trash-ovta-workflow-outlet.component.scss",
+    standalone: true,
 })
-export class TrashOvtaWorkflowOutletComponent {}
+export class TrashOvtaWorkflowOutletComponent implements OnInit {
+    public submitted: boolean = false;
+    public progress$: Observable<OnlandVisualTrashAssessmentWorkflowProgressDto>;
+    public OnlandVisualTrashAssessmentStatus = OnlandVisualTrashAssessmentStatusEnum;
+    @Input() onlandVisualTrashAssessmentID: number | null = null;
+
+    constructor(
+        private router: Router,
+        private ovtaProgressService: OvtaWorkflowProgressService,
+        private onlandVisualTrashAssessmentService: OnlandVisualTrashAssessmentService,
+        private confirmService: ConfirmService,
+        private alertService: AlertService,
+        private datePipe: DatePipe
+    ) {}
+
+    ngOnInit() {
+        this.progress$ = this.ovtaProgressService.progressObservable$.pipe(
+            tap(() => {
+                this.submitted = false;
+            })
+        );
+        this.ovtaProgressService.getProgress(this.onlandVisualTrashAssessmentID);
+    }
+
+    public deleteOVTA(onlandVisualTrashAssessmentID: number, createdDate: string) {
+        const modalContents = `<p>Are you sure you want to delete the assessment from ${this.datePipe.transform(createdDate, "MM/dd/yyyy")}?<\/p>`;
+        this.confirmService
+            .confirm({ buttonClassYes: "btn-primary", buttonTextYes: "Delete", buttonTextNo: "Cancel", title: "Delete OVTA", message: modalContents })
+            .then((confirmed) => {
+                if (confirmed) {
+                    this.onlandVisualTrashAssessmentService.onlandVisualTrashAssessmentsOnlandVisualTrashAssessmentIDDelete(onlandVisualTrashAssessmentID).subscribe(() => {
+                        this.alertService.clearAlerts();
+                        this.alertService.pushAlert(new Alert("Your OVTA was successfully deleted.", AlertContext.Success));
+                        this.router.navigate([`/trash/onland-visual-trash-assessments`]);
+                    });
+                }
+            });
+    }
+}
