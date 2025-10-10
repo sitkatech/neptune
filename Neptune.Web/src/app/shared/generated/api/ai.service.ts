fcc8c5a94569a0bfb9100b2d8f6c7748b0b1f1d9
raynardlee <raynardlee@yahoo.com>
2025-10-10 10:31:49 -0700

    NPT-976, NPT-982: removing extra extract-data endpoint

--- Neptune.Web/src/app/shared/generated/api/ai.service.ts
+++ Neptune.Web/src/app/shared/generated/api/ai.service.ts
@@ -99,58 +99,6 @@ export class AIService extends BaseService {
     }
 
     /**
-     * @param waterQualityManagementPlanDocumentID 
-     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
-     * @param reportProgress flag to report request and response progress.
-     */
-    public extractDataAI(waterQualityManagementPlanDocumentID: number, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: undefined, context?: HttpContext, transferCache?: boolean}): Observable<any>;
-    public extractDataAI(waterQualityManagementPlanDocumentID: number, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: undefined, context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<any>>;
-    public extractDataAI(waterQualityManagementPlanDocumentID: number, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: undefined, context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<any>>;
-    public extractDataAI(waterQualityManagementPlanDocumentID: number, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: undefined, context?: HttpContext, transferCache?: boolean}): Observable<any> {
-        if (waterQualityManagementPlanDocumentID === null || waterQualityManagementPlanDocumentID === undefined) {
-            throw new Error('Required parameter waterQualityManagementPlanDocumentID was null or undefined when calling extractDataAI.');
-        }
-
-        let localVarHeaders = this.defaultHeaders;
-
-        const localVarHttpHeaderAcceptSelected: string | undefined = options?.httpHeaderAccept ?? this.configuration.selectHeaderAccept([
-        ]);
-        if (localVarHttpHeaderAcceptSelected !== undefined) {
-            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
-        }
-
-        const localVarHttpContext: HttpContext = options?.context ?? new HttpContext();
-
-        const localVarTransferCache: boolean = options?.transferCache ?? true;
-
-
-        let responseType_: 'text' | 'json' | 'blob' = 'json';
-        if (localVarHttpHeaderAcceptSelected) {
-            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
-                responseType_ = 'text';
-            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
-                responseType_ = 'json';
-            } else {
-                responseType_ = 'blob';
-            }
-        }
-
-        let localVarPath = `/ai/water-quality-management-plan-documents/${this.configuration.encodeParam({name: "waterQualityManagementPlanDocumentID", value: waterQualityManagementPlanDocumentID, in: "path", style: "simple", explode: false, dataType: "number", dataFormat: "int32"})}/extract-data`;
-        const { basePath, withCredentials } = this.configuration;
-        return this.httpClient.request<any>('post', `${basePath}${localVarPath}`,
-            {
-                context: localVarHttpContext,
-                responseType: <any>responseType_,
-                ...(withCredentials ? { withCredentials } : {}),
-                headers: localVarHeaders,
-                observe: observe,
-                transferCache: localVarTransferCache,
-                reportProgress: reportProgress
-            }
-        );
-    }
-
-    /**
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
      */
